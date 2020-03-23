803544ba0130890239985f70dc87c6e4ccd96fef
Aen <hello@aentan.com>
2020-03-24 01:35:02 +0800

    syncing and spinner ui

--- src/components/SyncStatus/SyncStatus.js
+++ src/components/SyncStatus/SyncStatus.js
@@ -3,6 +3,7 @@ import {
   MdDone
 } from "react-icons/md";
 import styles from './SyncStatus.module.scss';
+import Spinner from '../../components/Spinner/Spinner';
 
 function StatusIcon(icon) {
   const StatusIcon = icon;
@@ -11,17 +12,33 @@ function StatusIcon(icon) {
 
 class SyncStatus extends Component {
   state = {
-    status: {
+    status: "Synced",
+    syncedAgo: "a minute ago",
+    statusAssets: {
       icon: MdDone,
-      label: 'Up-to-date'
+      label: "Synchronized"
     }
+  };
+
+  toggleStatus = () => {
+    this.setState({
+      status: this.state.status === "Synced" ? "Syncing" : "Synced",
+      syncedAgo: "just now",
+      statusAssets: {
+        icon: this.state.statusAssets.icon === MdDone ? Spinner : MdDone,
+        label: this.state.statusAssets.label === "Synchronized" ? "Syncing" : "Synchronized"
+      }
+    });
   };
 
   render() {
     return (
       <div className={styles.syncStatus}>
-        {this.state.status.label}
-        {StatusIcon(this.state.status.icon)}
+        <span onClick={this.toggleStatus}>
+          {this.state.statusAssets.label}
+          {this.state.status === "Synced" ? ` ${this.state.syncedAgo}` : ``}
+        </span>
+        {StatusIcon(this.state.statusAssets.icon)}
       </div>
     );
   }
