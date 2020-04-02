1d1d7d081aee933431a24c4714dfe977e1f7adb9
Harsh <fullstackninja864@gmail.com>
2020-04-02 10:59:52 +0530

    open debug mode on --debug argv

--- electron-start.js
+++ electron-start.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, protocol } = require('electron')
 
 const path = require('path')
 const url = require('url')
+const debug = /--debug/.test(process.argv[2]);
 
 let mainWindow
 
@@ -28,7 +29,9 @@ function createWindow() {
     })
   )
 
-  mainWindow.webContents.openDevTools()
+  if (debug) {
+    mainWindow.webContents.openDevTools()
+  }
 
 
   mainWindow.on('closed', () => {
