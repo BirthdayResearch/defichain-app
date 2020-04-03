4ca5c9d2adba9ac4fcd458bf808cc71bcbea66c9
Harsh <fullstackninja864@gmail.com>
2020-04-03 21:01:35 +0530

    i18 initial setup

--- src/app/rootStore.ts
+++ src/app/rootStore.ts
@@ -1,15 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import reducer from "./rootReducer";
 import sagaMiddleware, { startSaga } from "./rootSaga";
+import { setupI18n } from "../translations/i18n";
 
 const isProduction = process.env.NODE_ENV === "production";
-const middleware = isProduction ? [sagaMiddleware] : [sagaMiddleware, logger];
+const middleware = [...getDefaultMiddleware(), sagaMiddleware];
+
+if (!isProduction) {
+  middleware.push(logger);
+}
 
 const store = configureStore({
   reducer,
   middleware
 });
 startSaga();
+setupI18n(store);
 
 export default store;
