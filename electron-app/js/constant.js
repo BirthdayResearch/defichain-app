8495f6ec87d8f1c60c4847e0319f100d5e58f063
Harsh <fullstackninja864@gmail.com>
2020-04-17 16:17:33 +0530

    UI self config flow

--- electron-app/js/constant.js
+++ electron-app/js/constant.js
@@ -8,6 +8,10 @@ const PRELAUNCH_PREFERENCE_DISABLE = "prelaunch-preference-disable";
 
 const GET_CONFIG_DETAILS = "get-config-details";
 const CONFIG_FILE_NAME = path.join(app.getPath("home"), "./.defi/defi.conf");
+const UI_CONFIG_FILE_NAME = path.join(app.getPath("home"), "./.defi/defi.ui.yaml");
+const RANDOM_USERNAME_LENGTH = 8;
+const DEFAULT_RPC_BIND = "127.0.0.1";
+const DEFAULT_RPC_PORT = 8555;
 
 module.exports = {
   APP_NAME,
@@ -15,5 +19,9 @@ module.exports = {
   PRELAUNCH_PREFERENCE_ENABLE,
   PRELAUNCH_PREFERENCE_DISABLE,
   GET_CONFIG_DETAILS,
-  CONFIG_FILE_NAME
+  CONFIG_FILE_NAME,
+  UI_CONFIG_FILE_NAME,
+  RANDOM_USERNAME_LENGTH,
+  DEFAULT_RPC_BIND,
+  DEFAULT_RPC_PORT
 }
--- electron-app/js/ipcEvents/configDetails.js
+++ electron-app/js/ipcEvents/configDetails.js
@@ -1,7 +1,7 @@
 const { ipcMain } = require("electron");
 const { GET_CONFIG_DETAILS } = require("../constant");
-const { getConfig } = require("./../services/configDetails");
+const { getUiConfigDetails } = require("./../services/configDetails");
 
 ipcMain.on(GET_CONFIG_DETAILS, async (event, arg) => {
-  event.returnValue = await getConfig();
+  event.returnValue = await getUiConfigDetails();
 })
