6c5e3f375daf56c0ea3b39146c6d891c5c74201f
Harsh <fullstackninja864@gmail.com>
2020-04-17 16:17:33 +0530

    UI self config flow

--- electron-app/js/services/configDetails.js
+++ electron-app/js/services/configDetails.js
@@ -1,16 +1,110 @@
-const fs = require("fs");
 const ini = require("ini");
-const { responseMessage } = require("./../utils");
-const { CONFIG_FILE_NAME } = require("./../constant");
+const yaml = require("js-yaml");
+var randomString = require('random-string');
+const {
+  responseMessage,
+  getFileData,
+  checkFileExists,
+  writeFile,
+  getRpcAuth
+} = require("./../utils");
+const {
+  CONFIG_FILE_NAME,
+  UI_CONFIG_FILE_NAME,
+  DEFAULT_RPC_BIND,
+  DEFAULT_RPC_PORT,
+  RANDOM_USERNAME_LENGTH
+} = require("./../constant");
 
-const getConfig = async () => {
+const getUiConfigDetails = async () => {
   try {
-    const fileData = fs.readFileSync(CONFIG_FILE_NAME, "utf-8");
-    const configData = ini.parse(fileData)
+    // check for UI config file
+    if (checkFileExists(UI_CONFIG_FILE_NAME)) {
+      const configData = getUiConfig(UI_CONFIG_FILE_NAME)
+      return responseMessage(true, configData);
+    }
+
+    // check for default defi config paths
+    if (checkFileExists(CONFIG_FILE_NAME)) {
+      const defaultConfigData = getDefaultConfig();
+      const configData = saveUiConfigData(UI_CONFIG_FILE_NAME, defaultConfigData);
+      return responseMessage(true, configData);
+    }
+
+    // If default conf is not available
+    const username = randomString({
+      length: RANDOM_USERNAME_LENGTH,
+      numeric: false,
+      letters: true,
+      special: false,
+    });
+    const defaultConfig = {
+      "rpcauth": getRpcAuth(username),
+      "rpcbind": DEFAULT_RPC_BIND,
+      "rpcport": DEFAULT_RPC_PORT
+    }
+    const defaultConfigData = ini.encode(defaultConfig);
+    writeFile(CONFIG_FILE_NAME, defaultConfigData);
+    const configData = saveUiConfigData(UI_CONFIG_FILE_NAME, defaultConfig);
     return responseMessage(true, configData);
   } catch (err) {
+    console.log(err)
     return responseMessage(false, err);
   }
 }
 
-module.exports = { getConfig }
+
+const getUiConfig = (path) => {
+  const uiFileData = getFileData(path, "utf-8");
+  // TODO add UI yaml specific error message to inform user about corrupt yaml file -HARSH 
+  const configData = yaml.safeLoad(uiFileData);
+  const { rpcauth, rpcport, rpcconnect } = configData.remotes[0];
+
+  if (rpcauth && rpcport && rpcconnect) {
+    return configData;
+  }
+  throw new Error("Inconsistent data in UI config");
+}
+
+const getDefaultConfig = (path) => {
+  const fileData = getFileData(path, "utf-8");
+  // TODO add config specific error message to inform user about corrupt config file -HARSH 
+  const configData = ini.parse(fileData);
+  // check for required data in default config
+  const { rpcauth, rpcbind, rpcport, testnet, regnet } = configData;
+
+  if (rpcauth && rpcbind && rpcport) {
+    return {
+      rpcauth,
+      rpcbind,
+      rpcport,
+      testnet,
+      regnet
+    }
+  }
+  throw new Error("Inconsistent data in default config");
+}
+
+const saveUiConfigData = (path, configData) => {
+  const { rpcauth, rpcbind, rpcport, testnet, regnet } = configData;
+  const remotes = [];
+  const uiConfigData = {
+    rpcauth,
+    rpcport,
+    testnet,
+    regnet,
+    rpcconnect: rpcbind,
+  }
+  remotes.push(uiConfigData)
+  const yamlData = yaml.safeDump({ remotes }, {
+    "styles": {
+      "!!null": "lowercase" // dump null as ~
+    },
+    "skipInvalid": true,
+  });
+  // write to ui config file
+  writeFile(path, yamlData);
+  return uiConfigData;
+}
+
+module.exports = { getUiConfigDetails }
--- electron-app/js/utils.js
+++ electron-app/js/utils.js
@@ -1,10 +1,63 @@
+const fs = require("fs");
+const base64 = require("base-64");
+const utf8 = require("utf8");
+const cryptoJs = require("crypto-js");
+
 const responseMessage = (success, res) => {
   if (success) {
-    return { success: true, data: res }
+    return { success: true, data: res };
   }
-  return { success: false, message: res.message }
+  return { success: false, message: res.message };
 }
 
+// Check file exists or not 
+const checkFileExists = (path) => {
+  return path && fs.existsSync(path);
+}
+
+// Get file data
+const getFileData = (path, format) => {
+  try {
+    return fs.readFileSync(path, format);
+  } catch (err) {
+    throw err;
+  }
+}
+
+// write / append on UI config file
+const writeFile = (path, data, append) => {
+  try {
+    if (append && checkFileExists(path)) {
+      return fs.appendFileSync(path, data);
+    } else {
+      return fs.writeFileSync(path, data, 'utf8');
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+const generatePassword = () => {
+  //  Create 32 byte b64 password
+  const encoded = base64.encode(cryptoJs.lib.WordArray.random(32));
+  const bytes = base64.decode(encoded);
+  return utf8.decode(bytes);
+};
+
+
+//  get default RPC auth
+const getRpcAuth = (username) => {
+  const password = generatePassword();
+  const salt = cryptoJs.lib.WordArray.random(16);
+  const passwordHmac = cryptoJs.HmacSHA256(password, salt);
+  return `${username}:${salt}$${passwordHmac}`;
+};
+
 module.exports = {
-  responseMessage
+  responseMessage,
+  checkFileExists,
+  getFileData,
+  writeFile,
+  getRpcAuth
 }
