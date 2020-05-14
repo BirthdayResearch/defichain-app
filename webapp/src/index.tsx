a8115d15457e5aca2c41a60537d048de1ea62336
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2020-05-15 00:02:57 +0530

    Added basic schema validation ref (#19)

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app/App';
 import store from './app/rootStore';
-import './index.scss';
 
 ReactDOM.render(
   <Router>
---
+++ webapp/src/utils/schemas/rpcMethodSchemaMapping.ts
@@ -0,0 +1,19 @@
+import * as methodNames from '../../constants/rpcMethods';
+import * as rpcResponseSchemas from './rpcResponseSchema';
+
+export const rpcResponseSchemaMap = new Map<string, object>();
+
+rpcResponseSchemaMap.set(
+  methodNames.GET_BLOCKCHAIN_INFO,
+  rpcResponseSchemas.blockchainInfoSchema
+);
+
+rpcResponseSchemaMap.set(
+  methodNames.VALIDATE_ADDRESS,
+  rpcResponseSchemas.validAddressSchema
+);
+
+rpcResponseSchemaMap.set(
+  methodNames.LIST_RECEIVED_BY_ADDRESS,
+  rpcResponseSchemas.receiveAddressListSchema
+);
