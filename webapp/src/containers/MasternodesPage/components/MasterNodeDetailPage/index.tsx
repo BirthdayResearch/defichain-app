465607d5e48a4438e0b0837c839222e77ef4fa78
Harsh <fullstackninja864@gmail.com>
2020-06-18 17:40:49 +0530

    Add resign masternode

--- webapp/src/containers/MasternodesPage/components/MasterNodeDetailPage/index.tsx
+++ webapp/src/containers/MasternodesPage/components/MasterNodeDetailPage/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { isEmpty } from 'lodash';
 import { Helmet } from 'react-helmet';
+import classnames from 'classnames';
 import { I18n } from 'react-redux-i18n';
-import { Button } from 'reactstrap';
-import { MdArrowBack } from 'react-icons/md';
+import { Button, ButtonGroup, Row, Col } from 'reactstrap';
+import { MdArrowBack, MdDelete } from 'react-icons/md';
 import { NavLink, RouteComponentProps, Redirect } from 'react-router-dom';
 import KeyValueLi from '../../../../components/KeyValueLi';
 import { MASTER_NODES_PATH } from '../../../../constants';
 import { MasterNodeObject } from '../../masterNodeInterface';
+import { resignMasterNode } from '../../reducer';
 
 interface RouteProps {
   hash: string;
@@ -16,12 +18,13 @@ interface RouteProps {
 
 interface MasterNodeDetailPageProps extends RouteComponentProps<RouteProps> {
   masternodes: MasterNodeObject[];
+  resignMasterNode: (masterNodeHash: string) => void;
 }
 
 const MasterNodeDetailPage: React.FunctionComponent<MasterNodeDetailPageProps> = (
   props: MasterNodeDetailPageProps
 ) => {
-  const { match, masternodes = [] } = props;
+  const { match, masternodes = [], resignMasterNode } = props;
   const hash = match.params.hash;
   const masternode: any = masternodes.find((ele: any) => {
     return ele.hash && ele.hash.toString() === hash;
@@ -30,6 +33,11 @@ const MasterNodeDetailPage: React.FunctionComponent<MasterNodeDetailPageProps> =
   if (isEmpty(masternode)) {
     return <Redirect to={MASTER_NODES_PATH} />;
   }
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState<
+    boolean
+  >(false);
+  const [wait, setWait] = useState<number>(5);
+  const [allowCalls, setAllowCalls] = useState(false);
 
   const {
     ownerAuthAddress,
@@ -41,6 +49,23 @@ const MasterNodeDetailPage: React.FunctionComponent<MasterNodeDetailPageProps> =
     banTx,
     mintedBlocks,
   } = masternode || {};
+
+  useEffect(() => {
+    let waitToSendInterval;
+    if (isConfirmationModalOpen) {
+      let counter = 5;
+      waitToSendInterval = setInterval(() => {
+        counter -= 1;
+        setWait(counter);
+        if (counter === 0) {
+          clearInterval(waitToSendInterval);
+        }
+      }, 1000);
+    }
+    return () => {
+      clearInterval(waitToSendInterval);
+    };
+  }, [isConfirmationModalOpen]);
 
   return (
     <div className='main-wrapper'>
@@ -72,6 +97,16 @@ const MasterNodeDetailPage: React.FunctionComponent<MasterNodeDetailPageProps> =
           )}
           &nbsp;
         </h1>
+        <ButtonGroup>
+          <Button color='link' onClick={() => setIsConfirmationModalOpen(true)}>
+            <MdDelete />
+            <span>
+              {I18n.t(
+                'containers.masterNodes.masternodeDetailPage.resignMasterNode'
+              )}
+            </span>
+          </Button>
+        </ButtonGroup>
       </header>
       <div className='content'>
         <section className='mb-5'>
@@ -129,6 +164,52 @@ const MasterNodeDetailPage: React.FunctionComponent<MasterNodeDetailPageProps> =
           />
         </section>
       </div>
+      <footer className='footer-bar'>
+        <div
+          className={classnames({
+            'd-none': !isConfirmationModalOpen,
+          })}
+        >
+          <div className='footer-sheet'>
+            <dl className='row'>
+              <dd className='col-12'>
+                <span className='h2 mb-0'>
+                  {I18n.t(
+                    'containers.masterNodes.masternodeDetailPage.confirmation'
+                  )}
+                </span>
+              </dd>
+            </dl>
+          </div>
+          <Row className='justify-content-between align-items-center'>
+            <Col className='d-flex justify-content-end'>
+              <Button
+                color='link'
+                className='mr-3'
+                onClick={() => setIsConfirmationModalOpen(false)}
+              >
+                {I18n.t(
+                  'containers.masterNodes.masternodeDetailPage.noButtonText'
+                )}
+              </Button>
+              <Button
+                color='primary'
+                onClick={() => {
+                  setAllowCalls(true);
+                  resignMasterNode(hash);
+                }}
+                disabled={wait > 0 ? true : false}
+              >
+                {I18n.t(
+                  'containers.masterNodes.masternodeDetailPage.yesButtonText'
+                )}
+                &nbsp;
+                <span className='timer'>{wait > 0 ? wait : ''}</span>
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      </footer>
     </div>
   );
 };
@@ -142,4 +223,12 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(MasterNodeDetailPage);
+const mapDisptachToProps = {
+  resignMasterNode: (masterNodeHash: string) =>
+    resignMasterNode({ masterNodeHash }),
+};
+
+export default connect(
+  mapStateToProps,
+  mapDisptachToProps
+)(MasterNodeDetailPage);
--- webapp/src/containers/MasternodesPage/service.tsx
+++ webapp/src/containers/MasternodesPage/service.tsx
@@ -1,93 +1,14 @@
 import RpcClient from '../../utils/rpc-client';
 import { GET_NEW_ADDRESS_TYPE } from '../../constants';
 
-export const handelFetchMasterNodes = () => {
-  const data = {
-    result: {
-      '2a7ddddb0056bd814a580ec3a0420d6316a198e6ca60267f1d04cd6abe2ba31e': {
-        ownerAuthAddress: '7BQZ67KKYWSmVRukgv57m4HorjbGh7NWrQ',
-        operatorAuthAddress: '7GULFtS6LuJfJEikByKKg8psscg84jnfHs',
-        creationHeight: 0,
-        resignHeight: -1,
-        resignTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        banHeight: -1,
-        banTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        state: 'ENABLED',
-        mintedBlocks: 12008,
-      },
-      ec394fdd294275ec09579fe2cb9df1d4dff852d1457cf18985a8813986716861: {
-        ownerAuthAddress: '7E8Cjn9cqEwnrc3E4zN6c5xKxDSGAyiVUM',
-        operatorAuthAddress: '78MWNEcAAJxihddCw1UnZD8T7fMWmUuBro',
-        creationHeight: 0,
-        resignHeight: -1,
-        resignTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        banHeight: -1,
-        banTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        state: 'ENABLED',
-        mintedBlocks: 35649,
-      },
-      '33bd4108881acd5f106c9c43de160683b6f477a1e499cbeb5ccfea09dc46c874': {
-        ownerAuthAddress: 'tf1qxteztd6jez99gq0q3gxkm4lhsv00yxj9egmv9a',
-        operatorAuthAddress: 'tf1qxteztd6jez99gq0q3gxkm4lhsv00yxj9egmv9a',
-        creationHeight: 95655,
-        resignHeight: -1,
-        resignTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        banHeight: -1,
-        banTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        state: 'ENABLED',
-        mintedBlocks: 0,
-      },
-      d974445172d94e25468575ccbd3de28dd8c3231276e700b1b17f2e7589b3d1ac: {
-        ownerAuthAddress: '7GxxMCh7sJsvRK4GXLX5Eyh9B9EteXzuum',
-        operatorAuthAddress: '7MYdTGv3bv3z65ai6y5J1NFiARg8PYu4hK',
-        creationHeight: 0,
-        resignHeight: -1,
-        resignTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        banHeight: -1,
-        banTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        state: 'ENABLED',
-        mintedBlocks: 11879,
-      },
-      '0e252cecef2581006eb4bf503dd430c403f2f0a8b54519c2b222c601e0f0c2d2': {
-        ownerAuthAddress: '7LMorkhKTDjbES6DfRxX2RiNMbeemUkxmp',
-        operatorAuthAddress: '7KEu9JMKCx6aJ9wyg138W3p42rjg19DR5D',
-        creationHeight: 0,
-        resignHeight: -1,
-        resignTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        banHeight: -1,
-        banTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        state: 'ENABLED',
-        mintedBlocks: 38166,
-      },
-      e98f13531e1f6f7c2dccbe2bf3ac42dfc76e603cf00e8fa6dda3977ac99e08dd: {
-        ownerAuthAddress: '75SPByUNfab8GhLoaNET7Xa9f7pTJixx6B',
-        operatorAuthAddress: '73yXqLqJn4yNK7cGYxs2gzASdUtqNbDyCq',
-        creationHeight: 97692,
-        resignHeight: -1,
-        resignTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        banHeight: -1,
-        banTx:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-        state: 'ENABLED',
-        mintedBlocks: 0,
-      },
-    },
-  };
-  const transformedData = Object.keys(data.result).map(item => ({
+export const handelFetchMasterNodes = async () => {
+  const rpcClient = new RpcClient();
+  const masternodes = await rpcClient.listMasterNodes();
+  const transformedData = Object.keys(masternodes).map(item => ({
     hash: item,
-    ...data.result[item],
+    ...masternodes[item],
   }));
+
   return transformedData;
 };
 
