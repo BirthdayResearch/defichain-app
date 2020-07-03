52697bf313374d19d5ddecdbafd93d84ce79b718
Sumit Rajput <ssumitrajput854@gmail.com>
2020-07-03 21:01:53 +0530

    Added pre build scripts for all platforms

---
+++ pre-build-mac.sh
@@ -0,0 +1,12 @@
+mkdir -p binary
+cd binary && rm -rf win mac linux
+mkdir mac
+cd ..
+mkdir -p temp
+cd temp && rm -rf mac
+mkdir mac && cd mac
+wget https://github.com/DeFiCh/ain/releases/download/v1.0.0-rc1/defichain-1.0.0-rc1-x86_64-apple-darwin11.tar.gz
+tar -xvf defichain-1.0.0-rc1-x86_64-apple-darwin11.tar.gz
+cp defichain-1.0.0-rc1/bin/defid .
+cd ../.. && cp temp/mac/defid binary/mac/defid
+chmod 777 binary/linux/defid
