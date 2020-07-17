f0d84299918049ca7fe38324a4dbb244ec568141
Sumit Rajput <ssumitrajput854@gmail.com>
2020-07-17 12:23:20 +0530

    Added strategy to kill queue before closing app

--- webapp/src/worker/queue/index.ts
+++ webapp/src/worker/queue/index.ts
@@ -1,17 +1,26 @@
 import queue from 'async/queue';
+
 import { QUEUE_CONCURRENCY } from '../../constants';
+import { ipcRendererFunc } from '../../utils/isElectron';
+
+const ipcRenderer = ipcRendererFunc();
 
 const worker = (task, callback) => {
   task
     .methodName(...task.params)
-    .then(result => {
+    .then((result) => {
       callback(null, result);
     })
-    .catch(e => {
+    .catch((e) => {
       callback(e);
     });
 };
 
 const q = queue(worker, QUEUE_CONCURRENCY);
+
+ipcRenderer.on('kill-queue', () => {
+  ipcRenderer.removeAllListeners('kill-queue');
+  q.kill();
+});
 
 export default q;
