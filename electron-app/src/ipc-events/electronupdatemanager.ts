0afcf9b07d65772e5d0d6b288315a25d8b40f6ab
Sumit Rajput <ssumitrajput854@gmail.com>
2020-07-27 20:31:04 +0530

    Removed commented code

--- electron-app/src/ipc-events/electronupdatemanager.ts
+++ electron-app/src/ipc-events/electronupdatemanager.ts
@@ -60,7 +60,6 @@ export default function initiateElectronUpdateManager(autoUpdater: any) {
         setImmediate(() => {
           autoUpdater.quitAndInstall();
         })
-        // app.exit();
       }
     });
   });
