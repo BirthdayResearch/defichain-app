0e609b38e839e5398c3a21e1b64394aa959a9977
Harsh <fullstackninja864@gmail.com>
2020-08-05 13:04:10 +0530

    Add react md spinner in splash loader screen

--- webapp/src/components/LaunchScreen/index.tsx
+++ webapp/src/components/LaunchScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './LaunchScreen.module.scss';
-import { RiLoader4Line } from 'react-icons/ri';
+import Loader from '../Loader';
 import LaunchLogo from '../Svg/Launch';
 import { PACKAGE_VERSION, APP_TITLE } from '../../constants';
 
@@ -18,9 +18,7 @@ const LaunchScreen: React.FunctionComponent<LaunchScreenProps> = (
         <label className={style.appTitle}>{APP_TITLE}</label>
         <label className={style.subAppVersion}>{`v${PACKAGE_VERSION}`}</label>
         <label>{props.message}</label>
-        <div className={style.rotationconAnimation}>
-          <RiLoader4Line />
-        </div>
+        <Loader size={28} className='mt-5' />
       </div>
     </div>
   );
