b65393728731ab41fed4eeb461db50a0cc585313
Harsh <fullstackninja864@gmail.com>
2020-08-05 14:46:26 +0530

    Fix styles of color

--- webapp/src/components/Loader/index.tsx
+++ webapp/src/components/Loader/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MDSpinner from 'react-md-spinner';
+
 interface LoaderProps {
   size: number;
   color?: string;
@@ -11,8 +12,8 @@ const Loader: React.FunctionComponent<LoaderProps> = ({
   color,
   className,
 }) => (
-  <div className={className}>
-    <MDSpinner size={size} singleColor={color || '#000000'} borderSize={6} />
+  <div className={`${className || ''}`}>
+    <MDSpinner size={size} singleColor={color || '#6c6b6d'} borderSize={6} />
   </div>
 );
 
