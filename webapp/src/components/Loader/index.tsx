9ef20c21932e2fc3904330c5677e6904fc1a47af
Harsh <fullstackninja864@gmail.com>
2020-08-05 20:16:32 +0530

    Replace all loader with new loader design

--- webapp/src/components/Loader/index.tsx
+++ webapp/src/components/Loader/index.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import MDSpinner from 'react-md-spinner';
 
 interface LoaderProps {
-  size: number;
+  size?: number;
   color?: string;
   className?: string;
+  borderSize?: number;
 }
 
 const Loader: React.FunctionComponent<LoaderProps> = ({
   size,
   color,
   className,
+  borderSize,
 }) => (
   <div className={`${className || ''}`}>
-    <MDSpinner size={size} singleColor={color || '#6c6b6d'} borderSize={6} />
+    <MDSpinner
+      size={size || 28}
+      singleColor={color || '#6c6b6d'}
+      borderSize={borderSize || 6}
+    />
   </div>
 );
 
