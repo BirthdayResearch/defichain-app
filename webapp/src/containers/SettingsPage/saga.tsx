6fe459c24b5015c950c7a680d54045375b4b4030
Harsh <fullstackninja864@gmail.com>
2020-08-13 20:08:23 +0530

    WIP

--- webapp/src/containers/SettingsPage/saga.tsx
+++ webapp/src/containers/SettingsPage/saga.tsx
@@ -1,5 +1,5 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import * as  log from '../../utils/electronLogger';
+import { call, put, takeLatest, select, delay } from 'redux-saga/effects';
+import * as log from '../../utils/electronLogger';
 import {
   getSettingOptionsRequest,
   getSettingOptionsSuccess,
@@ -19,19 +19,40 @@ import {
   getLanguage,
   getAmountUnits,
   getDisplayModes,
+  getNetWorkList,
 } from './service';
 import store from '../../app/rootStore';
 import { setupI18n } from '../../translations/i18n';
 import { LANG_VARIABLE } from '../../constants';
 import PersistentStore from '../../utils/persistentStore';
+import { restartNode } from '../../utils/isElectron';
+import { restartModal } from '../ErrorModal/reducer';
+import q from '../../worker/queue';
+import {
+  MAINNET,
+  TESTNET,
+  REGTEST,
+  DEFAULT_MAINNET_CONNECT,
+  DEFAULT_TESTNET_CONNECT,
+  DEFAULT_REGTEST_CONNECT,
+  DEFAULT_MAINNET_PORT,
+  DEFAULT_TESTNET_PORT,
+  DEFAULT_REGTEST_PORT,
+} from '../../constants';
 
 export function* getSettingsOptions() {
   try {
     const languages = yield call(getLanguage);
     const amountUnits = yield call(getAmountUnits);
     const displayModes = yield call(getDisplayModes);
+    const networkOptions = yield call(getNetWorkList);
     yield put(
-      getSettingOptionsSuccess({ languages, amountUnits, displayModes })
+      getSettingOptionsSuccess({
+        languages,
+        amountUnits,
+        displayModes,
+        networkOptions,
+      })
     );
   } catch (e) {
     yield put(getSettingOptionsFailure(e.message));
@@ -40,10 +61,21 @@ export function* getSettingsOptions() {
 }
 
 export function* getSettings() {
+  const {
+    blockChainInfo: { chain },
+  } = yield select((state) => state.syncstatus);
+  let network = MAINNET;
+  console.log({ chain });
+  if (chain === 'test') {
+    network = TESTNET;
+  }
   try {
     const data = yield call(initialData);
     if (data) {
-      yield put({ type: getInitialSettingsSuccess.type, payload: { ...data } });
+      yield put({
+        type: getInitialSettingsSuccess.type,
+        payload: { ...data, network },
+      });
     } else {
       yield put({
         type: getInitialSettingsFailure.type,
@@ -73,6 +105,10 @@ export function* updateSettings(action) {
         disablePreLaunchStatus();
       }
       yield put({ type: updateSettingsSuccess.type, payload: { ...data } });
+      if (action.payload.network) {
+        console.log(action.payload.network);
+        yield call(changeNetworkNode, action.payload.network);
+      }
     } else {
       yield put({
         type: updateSettingsFailure.type,
@@ -83,6 +119,38 @@ export function* updateSettings(action) {
     yield put({ type: updateSettingsFailure.type, payload: e.message });
     log.error(e);
   }
+}
+
+function* changeNetworkNode(networkName) {
+  const { configurationData } = yield select((state) => state.app);
+  const network = {
+    regtest: 0,
+    testnet: 0,
+  };
+  let name = 'main';
+  const config = {
+    rpcbind: DEFAULT_MAINNET_CONNECT,
+    rpcport: DEFAULT_MAINNET_PORT,
+  };
+  if (networkName === TESTNET) {
+    network.testnet = 1;
+    name = 'test';
+    config.rpcbind = DEFAULT_TESTNET_CONNECT;
+    config.rpcport = DEFAULT_TESTNET_PORT;
+  }
+  // if (networkName === REGTEST) {
+  //   network.regtest = 1;
+  //   name = 'regtest';
+  //   config.rpcbind = DEFAULT_MAINNET_CONNECT;
+  //   config.rpcport = DEFAULT_MAINNET_PORT;
+  // }
+  const updatedConf = Object.assign({}, configurationData, network, {
+    [name]: config,
+  });
+  yield put(restartModal());
+  yield call(q.kill);
+  yield delay(2000);
+  yield call(restartNode, { updatedConf });
 }
 
 function* mySaga() {
