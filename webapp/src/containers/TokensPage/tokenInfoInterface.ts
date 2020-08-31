e815eda33e4f8b8b093cfd801694d605e33a69db
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-08-31 19:54:21 +0530

    Added Token interfaces

---
+++ webapp/src/containers/TokensPage/tokenInfoInterface.ts
@@ -0,0 +1,21 @@
+export interface DATTokenObject {
+  name: string;
+  icon: any;
+  symbol: string;
+  type: string;
+  price: string;
+  volume: string;
+  marketCap: string;
+  holders: string;
+}
+
+export interface DCTTokenObject {
+  name: string;
+  icon: any;
+  symbol: string;
+  id: string;
+  totalInitialSupply: string;
+  finalSupplyLimit: string;
+  mintingSupport: string;
+  tradeable: string;
+}
