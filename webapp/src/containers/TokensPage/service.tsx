712a8fa18243b3e9b555c492ec2280c3d5d95d9f
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-09-01 17:52:33 +0530

    Added service for tokenisation rpc calls

---
+++ webapp/src/containers/TokensPage/service.tsx
@@ -0,0 +1,94 @@
+import BitcoinIcon from '../../assets/svg/icon-coin-bitcoin-lapis.svg';
+import DeefIcon from '../../assets/svg/icon-coin-deef-lapis.svg';
+
+export const handleFetchToken = async (id: string) => {
+  // return dummy token data;
+  return {
+    name: 'Deef',
+    id: 'DV2XP9ECJ9LZZJP7LK0M',
+    symbol: 'DOO',
+    decimals: 18,
+    type: 'DCT (DeFi Custom Token)',
+    holders: '999,999',
+    price: '0.999 USD',
+    volume: '99,999.00 USD',
+    marketCap: '999,999.00 USD',
+    officialSite: 'https://eatdeef.com/',
+  };
+};
+
+export const handleFetchTokens = async () => {
+  // return dummy tokens data;
+  return [
+    {
+      name: 'DeFi Bitcoin',
+      icon: BitcoinIcon,
+      symbol: 'DBTC',
+      type: 'DAT',
+      price: '0.999 USD',
+      volume: '99,999 USD',
+      marketCap: '999,999 USD',
+      holders: '999,999',
+    },
+    {
+      name: 'DeFi Bitcoin',
+      icon: BitcoinIcon,
+      symbol: 'DBTC',
+      type: 'DAT',
+      price: '0.999 USD',
+      volume: '99,999 USD',
+      marketCap: '999,999 USD',
+      holders: '999,999',
+    },
+    {
+      name: 'DeFi Bitcoin',
+      icon: BitcoinIcon,
+      symbol: 'DBTC',
+      type: 'DAT',
+      price: '0.999 USD',
+      volume: '99,999 USD',
+      marketCap: '999,999 USD',
+      holders: '999,999',
+    },
+    {
+      name: 'DeFi Bitcoin',
+      icon: BitcoinIcon,
+      symbol: 'DBTC',
+      type: 'DAT',
+      price: '0.999 USD',
+      volume: '99,999 USD',
+      marketCap: '999,999 USD',
+      holders: '999,999',
+    },
+    {
+      name: 'DeFi Bitcoin',
+      icon: BitcoinIcon,
+      symbol: 'DBTC',
+      type: 'DAT',
+      price: '0.999 USD',
+      volume: '99,999 USD',
+      marketCap: '999,999 USD',
+      holders: '999,999',
+    },
+    {
+      name: 'Deef',
+      icon: DeefIcon,
+      symbol: 'DOO',
+      id: 'DV2XP9ECJ9LZZJP7LK0M',
+      totalInitialSupply: '99,999,999',
+      finalSupplyLimit: '999,999,999',
+      mintingSupport: 'Yes',
+      tradeable: 'Yes',
+    },
+    {
+      name: 'Deef',
+      icon: DeefIcon,
+      symbol: 'DOO',
+      id: 'DV2XP9ECJ9LZZJP7LK0M',
+      totalInitialSupply: '99,999,999',
+      finalSupplyLimit: '999,999,999',
+      mintingSupport: 'Yes',
+      tradeable: 'Yes',
+    },
+  ];
+};
