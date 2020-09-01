b8f3ac4c9c2f2a9ae1833c97de7fc3cdebc290f6
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-09-01 18:00:23 +0530

    Added token reducer and token saga in the root

--- webapp/src/app/rootSaga.ts
+++ webapp/src/app/rootSaga.ts
@@ -4,6 +4,7 @@ import startUpSaga from './startupSaga';
 import appSaga from '../containers/RpcConfiguration/saga';
 import blockchainSaga from '../containers/BlockchainPage/saga';
 import masterNodesSaga from '../containers/MasternodesPage/saga';
+import tokensSaga from '../containers/TokensPage/saga';
 import walletSaga from '../containers/WalletPage/saga';
 import settingsSaga from '../containers/SettingsPage/saga';
 import syncStatusSaga from '../containers/SyncStatus/saga';
@@ -15,6 +16,7 @@ function* rootSaga() {
     fork(appSaga),
     fork(blockchainSaga),
     fork(masterNodesSaga),
+    fork(tokensSaga),
     fork(walletSaga),
     fork(settingsSaga),
     fork(syncStatusSaga),
