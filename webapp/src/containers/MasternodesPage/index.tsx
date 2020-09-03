c79fa736e768601f2e81779ba1b9205763d59e80
Harsh <fullstackninja864@gmail.com>
2020-09-03 16:48:35 +0530

    Add a logic to hide tabs when there is no mine data present

--- webapp/src/containers/MasternodesPage/index.tsx
+++ webapp/src/containers/MasternodesPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { Button, ButtonGroup } from 'reactstrap';
 import { MdSearch, MdAdd } from 'react-icons/md';
@@ -7,21 +7,65 @@ import SearchBar from '../../components/SearchBar';
 import MasternodesList from './components/MasterNodesList';
 import { I18n } from 'react-redux-i18n';
 import { NavLink as RRNavLink, RouteComponentProps } from 'react-router-dom';
-import { CREATE_MASTER_NODES_PATH } from '../../constants';
+import { CREATE_MASTER_NODES_PATH, RESIGNED_STATE } from '../../constants';
 import MasternodeTab from './components/MasternodeTab';
+import { connect } from 'react-redux';
+import { fetchMasternodesRequest } from './reducer';
+import { MasterNodeObject } from './masterNodeInterface';
 
-const MasternodesPage: React.FunctionComponent<RouteComponentProps> = (
-  props: RouteComponentProps
+interface MasternodesPageProps extends RouteComponentProps {
+  masternodes: MasterNodeObject[];
+  fetchMasternodesRequest: () => void;
+  isLoadingMasternodes: boolean;
+}
+
+const MasternodesPage: React.FunctionComponent<MasternodesPageProps> = (
+  props: MasternodesPageProps
 ) => {
+  const { masternodes, fetchMasternodesRequest, isLoadingMasternodes } = props;
   const [searching, setSearching] = useState<boolean>(false);
   const [searchQuery, setSearchQuery] = useState<string>('');
-  const [activeTab, setActiveTab] = useState<string>('myMasternodes');
+  const [activeTab, setActiveTab] = useState<string>('otherMasterNodes');
+  const [disableTab, setDisableTab] = useState<boolean>(true);
+  const [enabledMasternodes, setEnabledMasternodes] = useState<
+    MasterNodeObject[]
+  >([]);
+
   const toggleSearch = () => {
     if (searching) {
       setSearchQuery('');
     }
     setSearching(!searching);
   };
+
+  useEffect(() => {
+    fetchMasternodesRequest();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoadingMasternodes) {
+      const myMasternodes = masternodes.filter(
+        (masternode) =>
+          masternode.state !== RESIGNED_STATE && masternode.isMyMasternode
+      );
+      if (myMasternodes.length > 0) {
+        setDisableTab(false);
+        setActiveTab('myMasternodes');
+      }
+    }
+  }, [isLoadingMasternodes]);
+
+  useEffect(() => {
+    const isMyMasternodes = activeTab === 'myMasternodes';
+    const enabledMasternodes = masternodes.filter((masternode) => {
+      if (isMyMasternodes) {
+        return masternode.state !== RESIGNED_STATE && masternode.isMyMasternode;
+      }
+      return masternode.state !== RESIGNED_STATE && !masternode.isMyMasternode;
+    });
+    setEnabledMasternodes(enabledMasternodes);
+  }, [activeTab]);
+
   return (
     <div className='main-wrapper'>
       <Helmet>
@@ -31,7 +75,9 @@ const MasternodesPage: React.FunctionComponent<RouteComponentProps> = (
         <h1 className={classnames({ 'd-none': searching })}>
           {I18n.t('containers.masterNodes.masterNodesPage.masterNodes')}
         </h1>
-        <MasternodeTab setActiveTab={setActiveTab} activeTab={activeTab} />
+        {!disableTab && (
+          <MasternodeTab setActiveTab={setActiveTab} activeTab={activeTab} />
+        )}
         <ButtonGroup className={classnames({ 'd-none': searching })}>
           <Button color='link' size='sm' onClick={toggleSearch}>
             <MdSearch />
@@ -57,6 +103,7 @@ const MasternodesPage: React.FunctionComponent<RouteComponentProps> = (
             activeTab={activeTab}
             searchQuery={searchQuery}
             history={props.history}
+            enabledMasternodes={enabledMasternodes}
           />
         </section>
       </div>
@@ -64,4 +111,16 @@ const MasternodesPage: React.FunctionComponent<RouteComponentProps> = (
   );
 };
 
-export default MasternodesPage;
+const mapStateToProps = (state) => {
+  const { masternodes, isLoadingMasternodes } = state.masterNodes;
+  return {
+    masternodes,
+    isLoadingMasternodes,
+  };
+};
+
+const mapDispatchToProps = {
+  fetchMasternodesRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MasternodesPage);
