87ab9f3e62e7c3ce77daab9db6f8063539943727
Harsh <fullstackninja864@gmail.com>
2020-09-16 11:28:54 +0530

    further refactor reindex component to index and component

--- webapp/src/containers/ErrorModal/ReIndexModel/index.tsx
+++ webapp/src/containers/ErrorModal/ReIndexModel/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { I18n } from 'react-redux-i18n';
-import { Button, ModalHeader } from 'reactstrap';
-import { Modal, ModalBody, ModalFooter } from 'reactstrap';
-import { MdClose } from 'react-icons/md';
+import { Modal } from 'reactstrap';
 import { restartNodeWithReIndexing, closeApp } from '../../../utils/isElectron';
 import { closeReIndexModal, isRestartLoader } from '../reducer';
-
+import ReIndexComponent from './ReIndexComponent';
 interface ReIndexModalProps {
   isReIndexModelOpen: boolean;
-  isReIndexRestart: boolean;
   closeReIndexModal: () => void;
   isRestartLoader: () => void;
 }
@@ -17,52 +13,36 @@ interface ReIndexModalProps {
 const ReIndexModal: React.FunctionComponent<ReIndexModalProps> = (
   props: ReIndexModalProps
 ) => {
+  const { closeReIndexModal, isRestartLoader, isReIndexModelOpen } = props;
+
   const restartAppWithReIndexing = () => {
-    props.closeReIndexModal();
-    props.isRestartLoader();
+    closeReIndexModal();
+    isRestartLoader();
     restartNodeWithReIndexing({ isReindexReq: true });
   };
 
   const closePopupAndApp = () => {
-    props.closeReIndexModal();
+    closeReIndexModal();
     closeApp();
   };
 
   return (
     <div>
-      <Modal isOpen={props.isReIndexModelOpen} centered>
-        <ModalHeader>
-          <MdClose onClick={props.closeReIndexModal} />
-        </ModalHeader>
-        <ModalBody>
-          <p className='text-center'>
-            {I18n.t('alerts.restartAppWithReindexNoticeline1')}
-          </p>
-          <p className='text-center'>
-            {I18n.t('alerts.restartAppWithReindexNoticeline2')}
-          </p>
-        </ModalBody>
-        <ModalFooter>
-          <div className='d-flex justify-content-end'>
-            <Button color='primary' onClick={restartAppWithReIndexing}>
-              {I18n.t('alerts.yesRestartAppWithReindex')}
-            </Button>
-            <Button className='ml-4' onClick={closePopupAndApp}>
-              {I18n.t('alerts.noCloseApp')}
-            </Button>
-          </div>
-        </ModalFooter>
+      <Modal isOpen={isReIndexModelOpen} centered>
+        <ReIndexComponent
+          restartAppWithReIndexing={restartAppWithReIndexing}
+          closePopupAndApp={closePopupAndApp}
+        />
       </Modal>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  const { isReIndexModelOpen, isReIndexRestart } = state.errorModal;
+  const { isReIndexModelOpen } = state.errorModal;
 
   return {
     isReIndexModelOpen,
-    isReIndexRestart,
   };
 };
 
