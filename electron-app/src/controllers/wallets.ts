a4c82c5f95bc489f3d131a28f5b0064936bc6fde
Harsh Rathi <fullstackninja864@gmail.com>
2020-09-21 20:06:26 +0530

    WIP:backup wallet.dat

--- electron-app/src/controllers/wallets.ts
+++ electron-app/src/controllers/wallets.ts
@@ -1,6 +1,14 @@
+import path from 'path';
 import * as log from '../services/electronLogger';
 import DialogManager from '../services/dialogmanager';
-import { MENU_BACKUP_WALLET, MENU_IMPORT_WALLET } from '../constants';
+import {
+  MENU_BACKUP_WALLET,
+  MENU_IMPORT_WALLET,
+  START_BACKUP_WALLET_DAT,
+  WALLET_DAT,
+} from '../constants';
+import { copyFile, getBaseFolder } from '../utils';
+import DefiProcessManager from '../services/defiprocessmanager';
 
 export default class Wallet {
   async load(bw: Electron.BrowserWindow) {
@@ -15,16 +23,30 @@ export default class Wallet {
 
   async backup(bw: Electron.BrowserWindow) {
     try {
-      const dialogManager = new DialogManager();
-      const paths = await dialogManager.saveFilePath();
-      if (!paths.length) {
-        throw new Error('No valid path available');
+      if (DefiProcessManager.isStartedNode) {
+        const dialogManager = new DialogManager();
+        const paths = await dialogManager.saveFilePath();
+        if (!paths.length) {
+          throw new Error('No valid path available');
+        }
+        bw.webContents.send(MENU_BACKUP_WALLET, { paths });
+      } else {
+        bw.webContents.send(START_BACKUP_WALLET_DAT);
       }
-      bw.webContents.send(MENU_BACKUP_WALLET, { paths });
       return true;
     } catch (err) {
       log.error(err);
       return false;
     }
+  }
+
+  async backupWalletDat() {
+    const dialogManager = new DialogManager();
+    const paths = await dialogManager.saveFilePath([
+      { name: 'Wallet', extensions: ['dat'] },
+    ]);
+    const baseFolder = getBaseFolder();
+    const src = path.join(baseFolder, WALLET_DAT);
+    return copyFile(src, paths);
   }
 }
