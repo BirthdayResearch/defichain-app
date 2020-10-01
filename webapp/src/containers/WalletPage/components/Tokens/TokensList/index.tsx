f6f561e8005820035bb191b7aaeac863c6e601a2
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-10-01 23:35:53 +0530

    Made changes for Token Balance and Transactions

--- webapp/src/containers/WalletPage/components/Tokens/TokensList/index.tsx
+++ webapp/src/containers/WalletPage/components/Tokens/TokensList/index.tsx
@@ -1,22 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink as RRNavLink, RouteComponentProps } from 'react-router-dom';
 import { I18n } from 'react-redux-i18n';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { MdAdd } from 'react-icons/md';
 import { Button, ButtonGroup } from 'reactstrap';
+import cloneDeep from 'lodash/cloneDeep';
 
+import {
+  fetchTokensRequest,
+  fetchAccountTokensRequest,
+} from '../../../reducer';
+import { filterByValue } from '../../../../../utils/utility';
 import {
   WALLET_ADD_TOKEN_PATH,
   WALLET_PAGE_PATH,
+  TOKEN_LIST_PAGE_SIZE,
+  DESTRUCTION_TX,
 } from '../../../../../constants';
 import WalletTokenCard from '../../../../../components/TokenCard/WalletTokenCard';
+import Pagination from '../../../../../components/Pagination';
 
-interface WalletTokensListProps extends RouteComponentProps {}
+interface WalletTokensListProps extends RouteComponentProps {
+  tokens: any;
+  accountTokens: any;
+  fetchTokensRequest: () => void;
+  fetchAccountTokensRequest: (ownerAddress) => void;
+}
 
 const WalletTokensList: React.FunctionComponent<WalletTokensListProps> = (
   props: WalletTokensListProps
 ) => {
+  const defaultPage = 1;
+  const [tableData, settableData] = useState([]);
+  const [currentPage, setCurrentPage] = useState<number>(defaultPage);
+  const pageSize = TOKEN_LIST_PAGE_SIZE;
+  const { fetchAccountTokensRequest, accountTokens } = props;
+  const total = accountTokens.length;
+  const pagesCount = Math.ceil(total / pageSize);
+  const from = (currentPage - 1) * pageSize;
+  const to = Math.min(total, currentPage * pageSize);
+
+  useEffect(() => {
+    fetchAccountTokensRequest({
+      ownerAddress: 'a91414e4641c4695acaf50738d88917242e5e6be603f87',
+    });
+  }, []);
+
+  function paginate(pageNumber, tokensList?: any[]) {
+    const clone = cloneDeep(tokensList || accountTokens);
+    const tableData = clone.slice(
+      (pageNumber - 1) * pageSize,
+      pageNumber * pageSize
+    );
+    setCurrentPage(pageNumber);
+    settableData(tableData);
+  }
+
+  useEffect(() => {
+    const tokensList: any[] = filterByValue(accountTokens, '');
+    paginate(defaultPage, tokensList);
+  }, [accountTokens]);
+
   const handleCardClick = () => {
     props.history.push(WALLET_PAGE_PATH);
   };
@@ -30,31 +75,60 @@ const WalletTokensList: React.FunctionComponent<WalletTokensListProps> = (
       </Helmet>
       <header className='header-bar'>
         <h1>{I18n.t('containers.wallet.walletTokensPage.tokens')}</h1>
-        <ButtonGroup>
+        {/* <ButtonGroup>
           <Button to={WALLET_ADD_TOKEN_PATH} tag={RRNavLink} color='link'>
             <MdAdd />
             <span className='d-lg-inline'>
               {I18n.t('containers.wallet.walletTokensPage.addToken')}
             </span>
           </Button>
-        </ButtonGroup>
+        </ButtonGroup> */}
       </header>
       <div className='content'>
-        <WalletTokenCard handleCardClick={handleCardClick} />
+        {tableData.map((token, index) => (
+          <WalletTokenCard
+            handleCardClick={handleCardClick}
+            key={index}
+            token={token}
+          />
+        ))}
+        <Pagination
+          label={I18n.t('containers.tokens.tokensPage.paginationRange', {
+            to,
+            total,
+            from: from + 1,
+          })}
+          currentPage={currentPage}
+          pagesCount={pagesCount}
+          handlePageClick={paginate}
+        />
       </div>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  const { tokens, isTokensLoaded, isLoadingTokens } = state.tokens;
+  const {
+    tokens,
+    isTokensLoaded,
+    isLoadingTokens,
+    accountTokens,
+    isAccountTokensLoaded,
+    isAccountLoadingTokens,
+  } = state.wallet;
   return {
     tokens,
     isTokensLoaded,
     isLoadingTokens,
+    accountTokens,
+    isAccountTokensLoaded,
+    isAccountLoadingTokens,
   };
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  fetchTokensRequest,
+  fetchAccountTokensRequest,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(WalletTokensList);
