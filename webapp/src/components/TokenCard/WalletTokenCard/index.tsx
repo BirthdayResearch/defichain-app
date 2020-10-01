c282f338982fda41ebd8c4dfe21400a6aa8a036e
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-10-01 23:35:53 +0530

    Made changes for Token Balance and Transactions

--- webapp/src/components/TokenCard/WalletTokenCard/index.tsx
+++ webapp/src/components/TokenCard/WalletTokenCard/index.tsx
@@ -5,13 +5,14 @@ import styles from '../TokenCard.module.scss';
 import Icon from '../../../assets/svg/icon-coin-bitcoin-lapis.svg';
 
 interface WalletTokenCardProps {
+  token: any;
   handleCardClick: (symbol: string) => void;
 }
 
 const WalletTokenCard: React.FunctionComponent<WalletTokenCardProps> = (
   props: WalletTokenCardProps
 ) => {
-  const { handleCardClick } = props;
+  const { handleCardClick, token } = props;
 
   return (
     <Card className={styles.tokenCard} onClick={() => handleCardClick('DAT')}>
@@ -19,22 +20,22 @@ const WalletTokenCard: React.FunctionComponent<WalletTokenCardProps> = (
         <Row>
           <Col md='6'>
             <div className='d-flex'>
-              <img src={Icon} />
+              {/* <img src={Icon} /> */}
               <div className='ml-4'>
                 <div>
-                  <b>{'DFI'}</b>
+                  <b>{token.symbol}</b>
                 </div>
-                <div className={styles.cardValue}>{'Defi Blockchain'}</div>
+                <div className={styles.cardValue}>{token.name}</div>
               </div>
             </div>
           </Col>
           <Col md='6'>
             <div className='float-right'>
               <div className={styles.cardValue}>
-                <b className='text-dark'>{'1,000'}</b>
-                <span className='ml-2'>{'DFI'}</span>
+                <b className='text-dark'>{token.amount}</b>
+                <span className='ml-2'>{token.symbol}</span>
               </div>
-              <div className={styles.cardValue}>{'220 USD'}</div>
+              {/* <div className={styles.cardValue}>{'220 USD'}</div> */}
             </div>
           </Col>
         </Row>
