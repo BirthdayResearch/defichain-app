79e07309e1bdb0f510cdedc63bb663c68de48ffc
Harsh Rathi <fullstackninja864@gmail.com>
2020-10-01 16:28:20 +0530

    Fix error message comming on the notification while restarting or shutdown of defi-app

--- webapp/src/containers/BlockchainPage/saga.tsx
+++ webapp/src/containers/BlockchainPage/saga.tsx
@@ -21,81 +21,72 @@ import {
   handleFetchBlockCount,
 } from './service';
 import store from '../../app/rootStore';
-import queue from '../../worker/queue';
+import { queuePush } from '../../utils/utility';
 
 function fetchBlocks(action) {
   const { currentPage: pageNo, pageSize } = action.payload;
-  queue.push(
-    { methodName: handelFetchBlocks, params: [pageNo, pageSize] },
-    (err, result) => {
-      if (err) {
-        store.dispatch(fetchBlocksFailure(err.message));
-        log.error(err);
-        return;
-      }
-      if (result && result.blocks)
-        store.dispatch(fetchBlocksSuccess({ ...result }));
-      else {
-        store.dispatch(fetchBlocksFailure('No data found'));
-      }
+  const callBack = (err, result) => {
+    if (err) {
+      store.dispatch(fetchBlocksFailure(err.message));
+      log.error(err);
+      return;
     }
-  );
+    if (result && result.blocks)
+      store.dispatch(fetchBlocksSuccess({ ...result }));
+    else {
+      store.dispatch(fetchBlocksFailure('No data found'));
+    }
+  };
+  queuePush(handelFetchBlocks, [pageNo, pageSize], callBack);
 }
 
 function fetchBlockData(action) {
   const { blockNumber } = action.payload;
-  queue.push(
-    { methodName: handleFetchBlockData, params: [blockNumber] },
-    (err, result) => {
-      if (err) {
-        store.dispatch(fetchBlockDataFailure(err.message));
-        log.error(err);
-        return;
-      }
-      if (result && result.blockData)
-        store.dispatch(fetchBlockDataSuccess({ ...result }));
-      else {
-        store.dispatch(fetchBlockDataFailure('No data found'));
-      }
+  const callBack = (err, result) => {
+    if (err) {
+      store.dispatch(fetchBlockDataFailure(err.message));
+      log.error(err);
+      return;
     }
-  );
+    if (result && result.blockData)
+      store.dispatch(fetchBlockDataSuccess({ ...result }));
+    else {
+      store.dispatch(fetchBlockDataFailure('No data found'));
+    }
+  };
+  queuePush(handleFetchBlockData, [blockNumber], callBack);
 }
 
 function fetchBlockCount() {
-  queue.push(
-    { methodName: handleFetchBlockCount, params: [] },
-    (err, result) => {
-      if (err) {
-        store.dispatch(fetchBlockCountFailure(err.message));
-        log.error(err);
-        return;
-      }
-      if (result && result.blockCount)
-        store.dispatch(fetchBlockCountSuccess({ ...result }));
-      else {
-        store.dispatch(fetchBlockCountFailure('No data found'));
-      }
+  const callBack = (err, result) => {
+    if (err) {
+      store.dispatch(fetchBlockCountFailure(err.message));
+      log.error(err);
+      return;
     }
-  );
+    if (result && result.blockCount)
+      store.dispatch(fetchBlockCountSuccess({ ...result }));
+    else {
+      store.dispatch(fetchBlockCountFailure('No data found'));
+    }
+  };
+  queuePush(handleFetchBlockCount, [], callBack);
 }
 
 function fetchTxns(action) {
   const { blockNumber, pageNo, pageSize } = action.payload;
-  queue.push(
-    { methodName: handelFetchTxns, params: [blockNumber, pageNo, pageSize] },
-    (err, result) => {
-      if (err) {
-        store.dispatch(fetchTxnsFailure(err.message));
-        log.error(err);
-        return;
-      }
-      if (result && result.txns)
-        store.dispatch(fetchTxnsSuccess({ ...result }));
-      else {
-        store.dispatch(fetchTxnsFailure('No data found'));
-      }
+  const callBack = (err, result) => {
+    if (err) {
+      store.dispatch(fetchTxnsFailure(err.message));
+      log.error(err);
+      return;
     }
-  );
+    if (result && result.txns) store.dispatch(fetchTxnsSuccess({ ...result }));
+    else {
+      store.dispatch(fetchTxnsFailure('No data found'));
+    }
+  };
+  queuePush(handelFetchTxns, [blockNumber, pageNo, pageSize], callBack);
 }
 
 function* mySaga() {
