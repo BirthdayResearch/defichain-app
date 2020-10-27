c247e0ad630433d16e88e9eabfafecda89e7ac48
Sumit Rajput <ssumitrajput854@gmail.com>
2020-10-27 20:03:18 +0530

    Add basic UI for pool tab page

--- webapp/src/containers/SwapPage/components/PoolTab/index.tsx
+++ webapp/src/containers/SwapPage/components/PoolTab/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { I18n } from 'react-redux-i18n';
+import { MdCompareArrows } from 'react-icons/md';
+
+import styles from './poolTab.module.scss';
 
 interface PoolTabProps {}
 
 const PoolTab: React.FunctionComponent<PoolTabProps> = (
   props: PoolTabProps
 ) => {
-  return <></>;
+  return (
+    <>
+      <section className={styles.sectionYourLliquidity}>{I18n.t('containers.swap.poolTab.yourLiquidity')}</section>
+      {true ? (
+        <div className='text-center'>
+          <MdCompareArrows size={50} className={styles.svg} />
+          <div>{I18n.t('containers.swap.poolTab.noLiquidity')}</div>
+          <div className={styles.labelAddLiquidity}>{I18n.t('containers.swap.poolTab.addLiquidity')}</div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default PoolTab;
