be14e2e57bd0c64b3a54b44a99a5430078597048
Sumit Rajput <ssumitrajput854@gmail.com>
2020-10-27 18:19:46 +0530

    Add basic UI for swap page

---
+++ webapp/src/components/SwapCard/index.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { I18n } from 'react-redux-i18n';
+import {
+  Row,
+  Card,
+  CardBody,
+  CardFooter,
+  Col,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Input,
+} from 'reactstrap';
+import styles from './SwapCard.module.scss';
+
+interface SwapCardProps {
+  label: string;
+  isFrom: boolean;
+  balance: number;
+  popularTokenList: string[];
+  normalTokenList: string[];
+}
+
+const SwapCard: React.FunctionComponent<SwapCardProps> = (
+  props: SwapCardProps
+) => {
+  const { isFrom, label, balance, popularTokenList, normalTokenList } = props;
+
+  const [fromAmount, setFromAmount] = useState(0);
+
+  const onInputChange = (e) => {
+    setFromAmount(e.target.value);
+  };
+
+  // temporary condition, need to update
+  const amount = isFrom ? fromAmount : Number(fromAmount) * 10 || 0;
+
+  return (
+    <Card className={styles.swapCard}>
+      <CardBody className={styles.cardBody}>
+        <Row>
+          <Col>{label}</Col>
+        </Row>
+        <Row>
+          <Col className='mt-2'>
+            {isFrom ? (
+              <Input
+                className='border-0'
+                type='text'
+                value={amount}
+                onChange={onInputChange}
+              ></Input>
+            ) : (
+              amount
+            )}
+          </Col>
+          <Col>
+            <UncontrolledDropdown>
+              <DropdownToggle caret color='outline-secondary'>
+                {I18n.t('components.swapCard.selectAToken')}
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>
+                  {I18n.t('components.swapCard.popular')}
+                  {popularTokenList.map((token) => (
+                    <DropdownItem>{token}</DropdownItem>
+                  ))}
+                </DropdownItem>
+                <DropdownItem header>
+                  {I18n.t('components.swapCard.tokens')}
+                  {normalTokenList.map((token) => (
+                    <DropdownItem>{token}</DropdownItem>
+                  ))}
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Col>
+        </Row>
+      </CardBody>
+      <CardFooter>
+        <Row>
+          <Col>
+            {I18n.t('components.swapCard.balance')}: {balance}
+          </Col>
+          {isFrom && (
+            <Col className={styles.colorMax}>
+              {I18n.t('components.swapCard.max')}
+            </Col>
+          )}
+        </Row>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default SwapCard;
