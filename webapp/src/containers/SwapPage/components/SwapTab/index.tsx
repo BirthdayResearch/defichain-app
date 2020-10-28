1a1b7b02d6070b26ccf2fee3236069c3db25ea50
Sumit Rajput <ssumitrajput854@gmail.com>
2020-10-28 19:40:11 +0530

    Update token dropdown list UI

--- webapp/src/containers/SwapPage/components/SwapTab/index.tsx
+++ webapp/src/containers/SwapPage/components/SwapTab/index.tsx
@@ -11,8 +11,15 @@ interface SwapTabProps {}
 const SwapTab: React.FunctionComponent<SwapTabProps> = (
   props: SwapTabProps
 ) => {
-  const popularTokenList = ['DFI', 'BTC', 'ETH'];
-  const normalTokenList = ['DOO', 'MEOW'];
+  const popularTokenList: Map<string, number> = new Map([
+    ['DFI', 10],
+    ['BTC', 20],
+    ['ETH', 30],
+  ]);
+  const normalTokenList: Map<string, number> = new Map([
+    ['DOO', 40],
+    ['MEOW', 50],
+  ]);
 
   return (
     <>
