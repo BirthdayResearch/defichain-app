d9534cbfd817de012c91275caa07a286a59a89c3
Sumit Rajput <ssumitrajput854@gmail.com>
2020-10-29 17:35:57 +0530

    Add searchbar component

---
+++ webapp/src/components/SwapSearchBar/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MdSearch } from 'react-icons/md';
+import {
+  Col,
+  FormGroup,
+  Input,
+  InputGroup,
+} from 'reactstrap';
+import styles from './SwapSearchBar.module.scss';
+
+interface SwapSearchBarProps {
+  searching: any;
+  // onChange: (e) => void;
+  placeholder?: string;
+}
+
+const SwapSearchBar: React.FunctionComponent<SwapSearchBarProps> = (
+  props: SwapSearchBarProps
+) => {
+  return (
+      <FormGroup className={`row ${styles.formGroup}`}>
+        <Col>
+          <InputGroup>
+            <Input
+              type='text'
+              placeholder={props.placeholder}
+              name='searchInput'
+              id='searchInput'
+              // onChange={props.onChange}
+            />
+            <MdSearch className={styles.searchIndicator} />
+          </InputGroup>
+        </Col>
+      </FormGroup>
+  );
+};
+
+export default SwapSearchBar;
