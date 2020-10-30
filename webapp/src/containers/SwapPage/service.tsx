e1386fc3a81b66764a5a82a578fa5822d70e3613
Sumit Rajput <ssumitrajput854@gmail.com>
2020-10-30 18:01:52 +0530

    Updated reducer saga and service

--- webapp/src/containers/SwapPage/service.tsx
+++ webapp/src/containers/SwapPage/service.tsx
@@ -1,14 +1,37 @@
 import { isEmpty } from 'lodash';
-import RpcClient from '../../utils/rpc-client';
+import _ from 'lodash';
 
-export const handleFetchPoolPairList = async () => {
+import RpcClient from '../../utils/rpc-client';
+import { handleFetchToken } from '../TokensPage/service';
+import { getAddressInfo } from '../WalletPage/service';
+
+export const handleFetchPoolshares = async () => {
   const rpcClient = new RpcClient();
-  const poolPairs = await rpcClient.listPoolPairs();
-  if (isEmpty(poolPairs)) {
+  const poolShares = await rpcClient.listPoolShares();
+  if (isEmpty(poolShares)) {
     return [];
   }
-  const transformedData = Object.keys(poolPairs).map((item) => ({
-    ...poolPairs[item],
-  }));
-  return transformedData;
+  const minePoolShares = Object.keys(poolShares).map(async (item) => {
+    const poolShare = poolShares[item];
+    const addressInfo = await getAddressInfo(poolShare.owner);
+
+    // if (addressInfo.ismine && !addressInfo.iswatchonly) {
+    if (true) {
+      const poolPair = await rpcClient.getPoolPair(poolShares[item].poolID);
+      const poolPairData = Object.keys(poolPair).map((item) => ({
+        hash: item,
+        ...poolPair[item],
+      }));
+      const tokenAData = await handleFetchToken(poolPairData[0].idTokenA);
+      const tokenBData = await handleFetchToken(poolPairData[0].idTokenB);
+      return {
+        tokenA: tokenAData.symbol,
+        tokenB: tokenBData.symbol,
+        ...poolPairData[0],
+        ...poolShares[item],
+      };
+    }
+  });
+
+  return _.compact(await Promise.all(minePoolShares));
 };
