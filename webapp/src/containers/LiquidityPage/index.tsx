18214269b4c833121887485d1cd298262f3c5555
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-11-17 19:19:06 +0530

    Added Liquidity tab in sidebar

---
+++ webapp/src/containers/LiquidityPage/index.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { I18n } from 'react-redux-i18n';
+import { MdAdd, MdCompareArrows } from 'react-icons/md';
+import { Button, ButtonGroup } from 'reactstrap';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import Helmet from 'react-helmet';
+
+import { CREATE_POOL_PAIR_PATH } from '../../constants';
+import styles from './liquidity.module.scss';
+import LiquidityList from './components/LiquidityList';
+import { fetchPoolPairListRequest, fetchPoolsharesRequest } from './reducer';
+import AvailablePoolPairsList from './components/AvailablePoolPairsList';
+
+interface LiquidityPageProps {
+  history: History;
+  poolshares: any;
+  poolPairList: any;
+  fetchPoolsharesRequest: () => void;
+  fetchPoolPairListRequest: () => void;
+}
+
+const LiquidityPage: React.FunctionComponent<LiquidityPageProps> = (
+  props: LiquidityPageProps
+) => {
+  const {
+    poolshares,
+    fetchPoolsharesRequest,
+    poolPairList,
+    fetchPoolPairListRequest,
+  } = props;
+
+  useEffect(() => {
+    fetchPoolsharesRequest();
+    fetchPoolPairListRequest();
+  }, []);
+
+  return (
+    <div className='main-wrapper'>
+      <Helmet>
+        <title>{I18n.t('containers.liquidity.liquidityPage.title')}</title>
+      </Helmet>
+      <header className='header-bar'>
+        <h1>{I18n.t('containers.liquidity.liquidityPage.liquidityPools')}</h1>
+        <ButtonGroup>
+          <Button to={CREATE_POOL_PAIR_PATH} tag={RRNavLink} color='link'>
+            <MdAdd />
+            <span className='d-lg-inline'>
+              {I18n.t('containers.liquidity.liquidityPage.addLiquidity')}
+            </span>
+          </Button>
+        </ButtonGroup>
+      </header>
+      <div className='content'>
+        <section className={`${styles.sectionYourLliquidity} mb-5`}>
+          {I18n.t('containers.liquidity.liquidityPage.yourLiquidity')}
+        </section>
+        {!poolshares.length ? (
+          <div className='text-center'>
+            <MdCompareArrows size={50} className={styles.svg} />
+            <div>
+              {I18n.t('containers.liquidity.liquidityPage.noLiquidity')}
+            </div>
+            <Button to={CREATE_POOL_PAIR_PATH} tag={RRNavLink} color='link'>
+              <div className={styles.labelAddLiquidity}>
+                {I18n.t('containers.liquidity.liquidityPage.addLiquidity')}
+              </div>
+            </Button>
+          </div>
+        ) : (
+          <LiquidityList poolshares={poolshares} history={props.history} />
+        )}
+        <div>
+          <section className={`${styles.sectionYourLliquidity} mb-5 mt-5`}>
+            {I18n.t('containers.liquidity.liquidityPage.availablePoolPairs')}
+          </section>
+          <AvailablePoolPairsList
+            searchQuery={''}
+            poolPairList={poolPairList}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const {
+    poolshares,
+    isPoolsharesLoaded,
+    isLoadingPoolshares,
+    poolPairList,
+  } = state.swap;
+  return {
+    poolPairList,
+    poolshares,
+    isPoolsharesLoaded,
+    isLoadingPoolshares,
+  };
+};
+
+const mapDispatchToProps = {
+  fetchPoolsharesRequest,
+  fetchPoolPairListRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LiquidityPage);
---
+++ webapp/src/containers/LiquidityPage/reducer.tsx
@@ -0,0 +1,164 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const initialState = {
+  poolshares: [],
+  isPoolsharesLoaded: false,
+  isLoadingPoolshares: false,
+  poolpair: {},
+  isLoadingPoolpair: false,
+  isPoolpairLoaded: false,
+  poolPairList: [],
+  isLoadingPoolPairList: false,
+  tokenBalanceList: [],
+  isLoadingAddPoolLiquidity: false,
+  isAddPoolLiquidityLoaded: false,
+  addPoolLiquidityHash: '',
+  isErrorAddingPoolLiquidity: '',
+  isLoadingRemovePoolLiquidity: false,
+  isRemovePoolLiquidityLoaded: false,
+  removePoolLiquidityHash: '',
+  isErrorRemovingPoolLiquidity: '',
+  testPoolSwap: '-',
+  isLoadingTestPoolSwap: false,
+  isTestPoolSwapLoaded: false,
+  isErrorTestPoolSwap: '',
+  poolSwapHash: '',
+  isLoadingPoolSwap: false,
+  isPoolSwapLoaded: false,
+  isErrorPoolSwap: '',
+};
+
+const configSlice = createSlice({
+  name: 'swap',
+  initialState,
+  reducers: {
+    fetchPoolpair(state, action) {
+      state.isLoadingPoolpair = true;
+    },
+    fetchPoolpairSuccess(state, action) {
+      state.poolpair = action.payload.poolpair;
+      state.isLoadingPoolpair = false;
+      state.isPoolpairLoaded = true;
+    },
+    fetchPoolpairFailure(state, action) {
+      state.poolpair = {};
+      state.isLoadingPoolpair = false;
+      state.isPoolpairLoaded = true;
+    },
+    fetchPoolsharesRequest(state) {
+      state.isLoadingPoolshares = true;
+    },
+    fetchPoolsharesSuccess(state, action) {
+      state.poolshares = action.payload.poolshares;
+      state.isLoadingPoolshares = false;
+      state.isPoolsharesLoaded = true;
+    },
+    fetchPoolsharesFailure(state) {
+      state.poolshares = [];
+      state.isLoadingPoolshares = false;
+      state.isPoolsharesLoaded = true;
+    },
+    fetchPoolPairListRequest(state) {
+      state.isLoadingPoolPairList = true;
+    },
+    fetchPoolPairListSuccess(state, action) {
+      state.poolPairList = action.payload;
+      state.isLoadingPoolPairList = false;
+    },
+    fetchPoolPairListFailure(state) {
+      state.isLoadingPoolPairList = false;
+    },
+    fetchTokenBalanceListRequest(state) {},
+    fetchTokenBalanceListSuccess(state, action) {
+      state.tokenBalanceList = action.payload;
+    },
+    fetchTokenBalanceListFailure(state) {},
+    addPoolLiquidityRequest(state, action) {
+      state.isLoadingAddPoolLiquidity = true;
+      state.isAddPoolLiquidityLoaded = false;
+    },
+    addPoolLiquiditySuccess(state, action) {
+      state.addPoolLiquidityHash = action.payload;
+      state.isLoadingAddPoolLiquidity = false;
+      state.isAddPoolLiquidityLoaded = true;
+    },
+    addPoolLiquidityFailure(state, action) {
+      state.isErrorAddingPoolLiquidity = action.payload;
+      state.isLoadingAddPoolLiquidity = false;
+      state.isAddPoolLiquidityLoaded = true;
+    },
+    removePoolLiqudityRequest(state, action) {
+      state.isLoadingRemovePoolLiquidity = true;
+      state.isRemovePoolLiquidityLoaded = false;
+    },
+    removePoolLiquiditySuccess(state, action) {
+      state.removePoolLiquidityHash = action.payload;
+      state.isLoadingRemovePoolLiquidity = false;
+      state.isRemovePoolLiquidityLoaded = true;
+    },
+    removePoolLiquidityFailure(state, action) {
+      state.isErrorRemovingPoolLiquidity = action.payload;
+      state.isLoadingRemovePoolLiquidity = false;
+      state.isRemovePoolLiquidityLoaded = true;
+    },
+    fetchTestPoolSwapRequest(state, action) {
+      state.isLoadingTestPoolSwap = true;
+    },
+    fetchTestPoolSwapSuccess(state, action) {
+      state.testPoolSwap = action.payload;
+      state.isLoadingTestPoolSwap = false;
+      state.isTestPoolSwapLoaded = true;
+    },
+    fetchTestPoolSwapFailure(state, action) {
+      state.testPoolSwap = '';
+      state.isLoadingTestPoolSwap = false;
+      state.isTestPoolSwapLoaded = true;
+      state.isErrorTestPoolSwap = action.payload;
+    },
+    poolSwapRequest(state, action) {
+      state.isLoadingPoolSwap = true;
+      state.isPoolSwapLoaded = false;
+    },
+    poolSwapSuccess(state, action) {
+      state.poolSwapHash = action.payload;
+      state.isLoadingPoolSwap = false;
+      state.isPoolSwapLoaded = true;
+    },
+    poolSwapFailure(state, action) {
+      state.isErrorPoolSwap = action.payload;
+      state.isLoadingPoolSwap = false;
+      state.isPoolSwapLoaded = true;
+    },
+  },
+});
+
+const { actions, reducer } = configSlice;
+
+export const {
+  fetchPoolpair,
+  fetchPoolpairSuccess,
+  fetchPoolpairFailure,
+  fetchPoolsharesRequest,
+  fetchPoolsharesSuccess,
+  fetchPoolsharesFailure,
+  fetchPoolPairListRequest,
+  fetchPoolPairListSuccess,
+  fetchPoolPairListFailure,
+  fetchTokenBalanceListRequest,
+  fetchTokenBalanceListSuccess,
+  fetchTokenBalanceListFailure,
+  addPoolLiquidityRequest,
+  addPoolLiquiditySuccess,
+  addPoolLiquidityFailure,
+  removePoolLiqudityRequest,
+  removePoolLiquiditySuccess,
+  removePoolLiquidityFailure,
+  fetchTestPoolSwapRequest,
+  fetchTestPoolSwapSuccess,
+  fetchTestPoolSwapFailure,
+  poolSwapRequest,
+  poolSwapSuccess,
+  poolSwapFailure,
+} = actions;
+
+export default reducer;
