51499a3b3f9e4bc95a42188f6bde5cd4ba8f719a
Harsh Rathi <fullstackninja864@gmail.com>
2020-11-18 20:08:27 +0530

    WIP add help Page

--- webapp/src/containers/HelpPage/index.tsx
+++ webapp/src/containers/HelpPage/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import {
+  FaGithub,
+  FaQuestionCircle,
+  FaReddit,
+  FaTelegramPlane,
+} from 'react-icons/fa';
 import { I18n } from 'react-redux-i18n';
 import Header from '../HeaderComponent';
+import { RouteComponentProps } from 'react-router-dom';
+import { Row, Col, Card, CardBody } from 'reactstrap';
+import styles from './HelpPage.module.scss';
 
-const HelpPage = () => {
+const HelpPage: React.FunctionComponent<RouteComponentProps> = (
+  props: RouteComponentProps
+) => {
   return (
     <div className='main-wrapper'>
       <Helmet>
@@ -13,7 +24,94 @@ const HelpPage = () => {
         <h1>{I18n.t('containers.helpPage.help')}</h1>
       </Header>
       <div className='content'>
-        <section>{I18n.t('containers.helpPage.section')}</section>
+        <section>
+          <Row>
+            <Col xs='12' md='6' className='my-2'>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col xs='3'>
+                      <FaTelegramPlane size='60px' />
+                    </Col>
+                    <Col xs='9'>
+                      <h4>{I18n.t('containers.helpPage.telegramEn')}</h4>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs='12' md='6' className='my-2'>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col xs='3'>
+                      <FaTelegramPlane size='60px' />
+                    </Col>
+                    <Col xs='9'>
+                      <h4>{I18n.t('containers.helpPage.telegramDe')}</h4>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs='12' md='6' className='my-2'>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col xs='3'>
+                      <FaGithub size='60px' />
+                    </Col>
+                    <Col xs='9'>
+                      <h4>{I18n.t('containers.helpPage.github')}</h4>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs='12' md='6' className='my-2'>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col xs='3'>
+                      <FaQuestionCircle size='60px' />
+                    </Col>
+                    <Col xs='9'>
+                      <h4>{I18n.t('containers.helpPage.faq')}</h4>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs='12' md='6' className='my-2'>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col xs='3'>
+                      <FaQuestionCircle size='60px' />
+                    </Col>
+                    <Col xs='9'>
+                      <h4>{I18n.t('containers.helpPage.defichainsite')}</h4>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs='12' md='6' className='my-2'>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col xs='3'>
+                      <FaReddit size='60px' />
+                    </Col>
+                    <Col xs='9'>
+                      <h4>{I18n.t('containers.helpPage.reddit')}</h4>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </section>
       </div>
     </div>
   );
