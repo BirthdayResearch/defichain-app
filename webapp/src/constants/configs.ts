b9834c73bfa236b5bb875dd8718f6f368e5af597
Sumit Rajput <ssumitrajput854@gmail.com>
2020-11-25 17:53:05 +0530

    Remove unused variables

--- webapp/src/constants/configs.ts
+++ webapp/src/constants/configs.ts
@@ -82,7 +82,6 @@ export const MASTERNODE_PARAMS_MASTERNODE_LIMIT = 1000;
 export const LP_DAILY_DFI_REWARD = 'LP_DAILY_DFI_REWARD';
 export const LP_SPLITS = 'LP_SPLITS';
 export const VS_CURRENCY = 'usd';
-export const DFI = 'DFI';
 export const BTC = 'BTC';
 
 export const DFI_SYMBOL = '0';
