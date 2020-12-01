c25c27fb287bbeccae058baca061b38e689e3e19
DeFiDev <defidev@cakedefi.com>
2020-12-01 18:24:11 +0700

    Added test for return customTx of type createToken

--- electron-app/src/services/__tests__/customTx.test.ts
+++ electron-app/src/services/__tests__/customTx.test.ts
@@ -1,5 +1,10 @@
-import { transaction, createTokenData, mintTokenData } from './testData.json';
-import { createZeroOutputTxFromCustomTx } from '../customTx';
+import {
+  transaction,
+  createTokenData,
+  mintTokenData,
+  utxo,
+} from './testData.json';
+import { createZeroOutputTxFromCustomTx, createTx } from '../customTx';
 
 describe('Custom tx', () => {
   it('should return object of transaction at type createToken', () => {
@@ -68,6 +73,41 @@ describe('Custom tx', () => {
         {
           satoshis: 0,
           script: '6a12446654784d01820000000000000000000000',
+        },
+      ],
+      version: 4,
+    });
+  });
+
+  it('should createCustomTx', async () => {
+    const tx = await createTx(
+      utxo,
+      'tttw7ZHJumuaLG8wSwQLDJ6B6jj2uqFnmR',
+      0,
+      createTokenData,
+      0
+    );
+    expect(tx.toObject()).toEqual({
+      hash: 'fb04d5844c85ab6407de91f74902557fe9306b57b1e4e927c641df2fba5a5928',
+      inputs: [
+        {
+          outputIndex: 0,
+          prevTxId:
+            '983afbde01a436823054aae561f973e4261ce1e331482eb85c462981e99138a3',
+          script: '',
+          scriptString: '',
+          sequenceNumber: 4294967295,
+        },
+      ],
+      nLockTime: 0,
+      outputs: [
+        {
+          satoshis: 0,
+          script: 'a914f844de65447a18e05384425a68b56813c20d99cf87',
+        },
+        {
+          satoshis: 0,
+          script: '6a1944665478540841686d6564546f6b0008000000000000000003',
         },
       ],
       version: 4,
