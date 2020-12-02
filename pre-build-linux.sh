d4cf8a12df2f869e6a0c78533c3a705788bc1429
Sumit Rajput <ssumitrajput854@gmail.com>
2020-12-02 21:42:33 +0530

    Update scripts to use node version "1.3.8"

--- pre-build-linux.sh
+++ pre-build-linux.sh
@@ -5,8 +5,8 @@ cd ..
 mkdir -p temp
 cd temp && rm -rf linux
 mkdir linux && cd linux
-wget https://github.com/DeFiCh/ain/releases/download/v1.3.7/defichain-1.3.7-x86_64-pc-linux-gnu.tar.gz
-tar -xvf defichain-1.3.7-x86_64-pc-linux-gnu.tar.gz
-cp defichain-1.3.7/bin/defid .
+wget https://github.com/DeFiCh/ain/releases/download/v1.3.8/defichain-1.3.8-x86_64-pc-linux-gnu.tar.gz
+tar -xvf defichain-1.3.8-x86_64-pc-linux-gnu.tar.gz
+cp defichain-1.3.8/bin/defid .
 cd ../.. && cp temp/linux/defid binary/linux/defid
 chmod 777 binary/linux/defid
