58d6369a67dfbcd48a28d48ab44c43495f049894
Sumit Rajput <ssumitrajput854@gmail.com>
2020-12-02 11:39:49 +0530

    Handle gateway timeout error for stats api

--- webapp/src/constants/configs.ts
+++ webapp/src/constants/configs.ts
@@ -98,6 +98,7 @@ export const COINGECKO_BTC_ID = 'bitcoin';
 export const COINGECKO_ETH_ID = 'ethereum';
 export const COINGECKO_USDT_ID = 'tether';
 
+export const API_REQUEST_TIMEOUT = 5000;
 export const STATS_API_BASE_URL = 'https://api.defichain.io/v1/getblockcount';
 export const COINGECKO_API_BASE_URL = 'https://api.coingecko.com/api/v3';
 export const TELEGRAM_GERMAN_HELP_LINK = 'https://t.me/defiblockchain_DE';
