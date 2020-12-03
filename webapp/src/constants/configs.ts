50c4147332ec2d3e059fb852b4d98c5c09338e19
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-12-03 16:01:04 +0530

    Add Icon for AccountToUTXOs type

--- webapp/src/constants/configs.ts
+++ webapp/src/constants/configs.ts
@@ -144,5 +144,6 @@ export const TX_TYPES = {
 export const RECIEVE_CATEGORY_LABEL = 'Receive';
 export const SENT_CATEGORY_LABEL = 'Sent';
 export const TRANSFER_CATEGORY_LABEL = 'Transfer';
+export const ACCOUNT_TO_UTXOS_LABEL = 'AccountToUtxos';
 export const REWARDS_CATEEGORY_LABEL = TX_TYPES.NonTxRewards;
 export const POOL_SWAP_CATEGORY_LABEL = TX_TYPES.PoolSwap;
