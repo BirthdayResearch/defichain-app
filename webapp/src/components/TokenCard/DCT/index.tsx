2c00e8d6955952933cdc56f7820b09ce34e3a5eb
Aen <aentan@users.noreply.github.com>
2020-12-07 19:23:15 +0800

    Lp ui fixes 1 dec (#72)
    * add react-icons back to packages
    
    * Fix buttons, form controls, menu active background and colors
    
    * Fix wrong text, text case and back button case
    
    * Fix DEX welcome page icons
    
    * fix Add/Remove liquidity UTXO progress styles
    
    * Improve token avatar
    
    * Change ETH icon
    
    Co-authored-by: Sumit Rajput <ssumitrajput854@gmail.com>

--- webapp/src/components/TokenCard/DCT/index.tsx
+++ webapp/src/components/TokenCard/DCT/index.tsx
@@ -17,20 +17,28 @@ const TokenCard: React.FunctionComponent<ITokenCard> = (props: ITokenCard) => {
       onClick={() => props.handleCardClick(data.symbol, data.hash)}
     >
       <CardBody className={styles.cardBody}>
-        <Row className='mb-3'>
-          <Col>
-            <b>
-              {data.isLPS
+        <Row className='mb-2'>
+          <Col md='8' className='d-flex align-items-center'>
+            <div>
+              <span className={styles.tokenName}>
+                {data.isLPS
                 ? `${I18n.t(
                     'containers.tokens.tokensPage.dctLabels.liquidityTokenFor'
                   )} ${data.symbolKey}`
                 : data.name}
-            </b>
-            <span>{data.symbolKey}</span>
+              </span>{' '}
+              <span className={styles.tokenSymbol}>{data.symbolKey}</span>
+            </div>
           </Col>
-          <Col md='6' className={`${styles.tokenCardIcon}`}>
-            {data.isDAT && <img src={VerifiedIcon} className='mr-1' />}
-            <TokenAvatar symbol={data.symbolKey} />
+          <Col md='4' className={`${styles.tokenCardIcon}`}>
+            {data.isDAT && (
+              <img src={VerifiedIcon} className={styles.verifiedIcon} />
+            )}
+            <TokenAvatar
+              symbol={data.symbolKey}
+              size='32px'
+              textSizeRatio={2}
+            />
           </Col>
         </Row>
         {/* <Row>
