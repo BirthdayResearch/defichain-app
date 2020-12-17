2189b314846cbf09dd4eecbaa07eeb2c3dc91017
Sumit Rajput <ssumitrajput854@gmail.com>
2020-12-17 16:41:34 +0530

    Update defi binary version to "1.3.15"

--- pre-build-linux.sh
+++ pre-build-linux.sh
@@ -5,8 +5,8 @@ cd ..
 mkdir -p temp
 cd temp && rm -rf linux
 mkdir linux && cd linux
-wget https://github.com/DeFiCh/ain/releases/download/v1.3.14/defichain-1.3.14-x86_64-pc-linux-gnu.tar.gz
-tar -xvf defichain-1.3.14-x86_64-pc-linux-gnu.tar.gz
-cp defichain-1.3.14/bin/defid .
+wget https://github.com/DeFiCh/ain/releases/download/v1.3.15/defichain-1.3.15-x86_64-pc-linux-gnu.tar.gz
+tar -xvf defichain-1.3.15-x86_64-pc-linux-gnu.tar.gz
+cp defichain-1.3.15/bin/defid .
 cd ../.. && cp temp/linux/defid binary/linux/defid
 chmod 777 binary/linux/defid
