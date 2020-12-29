9c5f3e3a618f35e658f777961be7b781411378d8
JJ Adonis <josejaimecareers@gmail.com>
2020-12-29 19:28:39 +0800

    chore: removed neutrino

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -1,5 +1,16 @@
-const neutrino = require('neutrino');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'test';
-
-module.exports = neutrino().jest();
+module.exports = {
+    testEnvironment: 'jsdom',
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
+    setupFilesAfterEnv: [
+        '<rootDir>/test/setupTests.ts',
+        '<rootDir>/test/setupRpcInitialData.ts',
+    ],
+    setupFiles: ['<rootDir>/test/mockLocalStorage.js'],
+    transform: {
+        '^.+\\.[ts|tsx]?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+};
