103efd0930b085824c15d35ee7ae4010f6011b85
Sumit <sumit@cakedefi.com>
2020-12-30 13:37:41 +0530

    Fix existing test cases

--- webapp/src/containers/PopOver/__test__/PostUpdateComponent.test.tsx
+++ webapp/src/containers/PopOver/__test__/PostUpdateComponent.test.tsx
@@ -1,12 +1,12 @@
-import { mount } from 'enzyme';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../../../app/rootStore';
 import PostUpdateComponent from '../UpdateProgress/PostUpdateComponent';
+import Enzyme from '../../../utils/testUtils/enzyme';
 
 describe('PostUpdateComponent', () => {
   it('should check for snapshot', () => {
-    const wrapper = mount(
+    const wrapper = Enzyme.mount(
       <Provider store={store}>
         <PostUpdateComponent closeModal={() => {}} />
       </Provider>
