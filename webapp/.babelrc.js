bf0d1dba7780da8211bf314f7bd6c55b19cca87b
JJ Adonis <josejaimecareers@gmail.com>
2020-12-31 19:50:53 +0800

    chore: added browserlist

--- webapp/.babelrc.js
+++ webapp/.babelrc.js
@@ -6,20 +6,7 @@ module.exports = (api) => {
     api.cache.using(() => mode);
 
     return {
-        presets: [
-            [
-                '@babel/preset-env',
-                {
-                    targets: {
-                        browsers: ['>1%', 'last 4 versions', 'not ie < 9'],
-                    },
-                    useBuiltIns: 'usage',
-                    debug: false,
-                    corejs: 3,
-                },
-            ],
-            '@babel/preset-react',
-        ],
+        presets: ['@babel/preset-env', '@babel/preset-react'],
         plugins: [
             '@babel/plugin-syntax-dynamic-import',
             '@babel/plugin-proposal-class-properties',
