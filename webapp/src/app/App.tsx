288c733ef5a97b3b30272fc0d355c29a14fbc25d
JJ Adonis <josejaimecareers@gmail.com>
2021-01-04 15:28:05 +0800

    chore: updated tabwidth 2 to spaces

--- webapp/src/app/App.tsx
+++ webapp/src/app/App.tsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useRef} from 'react';
-import {Helmet} from 'react-helmet';
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
-import {TransitionGroup, CSSTransition} from 'react-transition-group';
-import {RouteComponentProps} from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { Helmet } from 'react-helmet';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { RouteComponentProps } from 'react-router-dom';
 import './App.scss'; // INFO: do not move down, placed on purpose
 import Sidebar from '../containers/Sidebar';
-import {getRpcConfigsRequest} from '../containers/RpcConfiguration/reducer';
+import { getRpcConfigsRequest } from '../containers/RpcConfiguration/reducer';
 import ErrorModal from '../containers/PopOver/ErrorModal';
 import routes from '../routes';
 import LaunchScreen from '../components/LaunchScreen';
@@ -22,62 +22,62 @@ import { getPageTitle } from '../utils/utility';
 import RefreshUtxosModal from '../containers/PopOver/RefreshUtxosModal';
 
 interface AppProps extends RouteComponentProps {
-    isRunning: boolean;
-    getRpcConfigsRequest: () => void;
-    nodeError: string;
-    isFetching: boolean;
-    isRestart: boolean;
+  isRunning: boolean;
+  getRpcConfigsRequest: () => void;
+  nodeError: string;
+  isFetching: boolean;
+  isRestart: boolean;
 }
 
 const getPathDepth = (location: any): number => {
-    return (location || {}).pathname.split('/').length;
+  return (location || {}).pathname.split('/').length;
 };
 
 const determineTransition = (location, prevDepth) => {
-    const depth = getPathDepth(location) - prevDepth;
+  const depth = getPathDepth(location) - prevDepth;
 
-    if (depth < 0) {
-        return ['transit-pop', 300];
-    } else if (depth > 0) {
-        return ['transit-push', 300];
-    } else {
-        return ['transit-fade', 30];
-    }
+  if (depth < 0) {
+    return ['transit-pop', 300];
+  } else if (depth > 0) {
+    return ['transit-push', 300];
+  } else {
+    return ['transit-fade', 30];
+  }
 };
 
 const App: React.FunctionComponent<AppProps> = (props: AppProps) => {
-    const {
-        location,
-        getRpcConfigsRequest,
-        isRunning,
-        nodeError,
-        isFetching,
-        isRestart,
-    } = props;
+  const {
+    location,
+    getRpcConfigsRequest,
+    isRunning,
+    nodeError,
+    isFetching,
+    isRestart,
+  } = props;
 
-    const prevDepth = useRef(getPathDepth(location));
+  const prevDepth = useRef(getPathDepth(location));
 
-    useEffect(() => {
-        getRpcConfigsRequest();
-    }, []);
+  useEffect(() => {
+    getRpcConfigsRequest();
+  }, []);
 
-    useEffect(() => {
-        prevDepth.current = getPathDepth(location);
-    });
+  useEffect(() => {
+    prevDepth.current = getPathDepth(location);
+  });
 
-    const transition = determineTransition(location, prevDepth.current);
+  const transition = determineTransition(location, prevDepth.current);
 
   return (
     <>
       {isRunning ? (
-        <div id='app'>
+        <div id="app">
           <Helmet>
             <title>{getPageTitle()}</title>
           </Helmet>
           <Sidebar />
           <main>
             <TransitionGroup
-              className='transition-group'
+              className="transition-group"
               childFactory={(child) =>
                 React.cloneElement(child, {
                   classNames: transition[0],
@@ -111,13 +111,13 @@ const App: React.FunctionComponent<AppProps> = (props: AppProps) => {
   );
 };
 
-const mapStateToProps = ({app, popover}) => ({
-    isRunning: app.isRunning,
-    nodeError: app.nodeError,
-    isFetching: app.isFetching,
-    isRestart: popover.isReIndexRestart,
+const mapStateToProps = ({ app, popover }) => ({
+  isRunning: app.isRunning,
+  nodeError: app.nodeError,
+  isFetching: app.isFetching,
+  isRestart: popover.isReIndexRestart,
 });
 
-const mapDispatchToProps = {getRpcConfigsRequest};
+const mapDispatchToProps = { getRpcConfigsRequest };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
