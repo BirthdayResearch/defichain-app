30a311ad6b4ffc4b5d023b363c9a319077855bc9
Saurabh Shinde <saurabhshinde391@gmail.com>
2021-01-13 16:03:59 +0530

    remove duplicate page of walletPage

--- webapp/src/containers/WalletPage/__tests__/SendPage.test.tsx
+++ webapp/src/containers/WalletPage/__tests__/SendPage.test.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
-import SendPage from '../components/SendPage/index';
 import { BrowserRouter as Router } from 'react-router-dom';
+import SendPage from '../components/SendPage';
 import { Provider } from 'react-redux';
 import store from '../../../app/rootStore';
-import Enzyme from '../../../utils/testUtils/enzyme';
-import { location } from '../../../utils/testUtils/routeComponentProps';
+import { mount } from 'enzyme';
+import * as routeComponentProps from '../../../utils/testUtils/routeComponentProps';
 
 describe('SendPage component', () => {
   it('should check for snapshot', () => {
-    const props = {
-      unit: '123',
-      location: location,
-      sendData: {
-        walletBalance: 123,
-        amountToSend: 'amountToSend',
-        amountToSendDisplayed: 'amountToSendDisplayed',
-        toAddress: 'toAddress',
-        scannerOpen: true,
-        flashed: 'flashed',
-        showBackdrop: 'showBackdrop',
-        sendStep: 'sendStep',
-        waitToSend: 1,
-      },
-      fetchSendDataRequest: () => {},
-    };
-    const wrapper = Enzyme.mount(
+    const wrapper = mount(
       <Router>
         <Provider store={store}>
-          <SendPage {...props} />
+          <SendPage {...routeComponentProps} />
         </Provider>
       </Router>
     );
