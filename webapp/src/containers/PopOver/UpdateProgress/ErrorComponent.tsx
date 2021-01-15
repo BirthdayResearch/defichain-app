670d788f29386fa8429e223bd05c485e7ee121b1
JJ Adonis <josejaimecareers@gmail.com>
2021-01-15 13:24:20 +0800

    feat: added download button on error

--- webapp/src/containers/PopOver/UpdateProgress/ErrorComponent.tsx
+++ webapp/src/containers/PopOver/UpdateProgress/ErrorComponent.tsx
@@ -3,6 +3,8 @@ import { Button, ModalBody, ModalFooter } from 'reactstrap';
 import { connect } from 'react-redux';
 import { I18n } from 'react-redux-i18n';
 import { closeUpdateApp } from '../reducer';
+import openNewTab from 'src/utils/openNewTab';
+import { SITE_DOWNLOADS } from '@defi_types/settings';
 
 interface DownloadProgressComponentProps {
   isUpdateError: string;
@@ -11,10 +13,17 @@ interface DownloadProgressComponentProps {
 
 const DownloadProgressComponent = (props: DownloadProgressComponentProps) => {
   const { isUpdateError, closeUpdateApp } = props;
+  const onDownloadClick = () => {
+    openNewTab(SITE_DOWNLOADS);
+    closeUpdateApp();
+  };
   return (
     <>
       <ModalBody>{isUpdateError}</ModalBody>
       <ModalFooter>
+        <Button size='sm' color='link' onClick={onDownloadClick}>
+          {I18n.t('alerts.download')}
+        </Button>
         <Button size='sm' onClick={closeUpdateApp}>
           {I18n.t('alerts.closeBtnLabel')}
         </Button>
