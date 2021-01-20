10b87cfbee297c20821b7f3ca7fd2f2cf2218a73
Saurabh Shinde <saurabhshinde391@gmail.com>
2021-01-20 23:09:02 +0530

    Correct module path of MintToken and Tokeninfo , CreateDCT->Footer components

--- webapp/src/containers/TokensPage/components/TokenInfo/index.tsx
+++ webapp/src/containers/TokensPage/components/TokenInfo/index.tsx
@@ -38,7 +38,7 @@ import { ITokenResponse } from '../../../../utils/interfaces';
 import TokenAvatar from '../../../../components/TokenAvatar';
 import { getIcon, getPageTitle } from '../../../../utils/utility';
 import Header from '../../../HeaderComponent';
-import ViewOnChain from 'src/components/ViewOnChain';
+import ViewOnChain from '../../../../../src/components/ViewOnChain';
 
 interface RouteParams {
   id?: string;
