7589790e29f4a6154b1951d925c05f4e8aee0bd4
Saurabh Shinde <saurabhshinde391@gmail.com>
2021-01-20 22:47:31 +0530

    Add snapshot and test case, correct  module path of sidebar and SwapPage page

--- webapp/src/containers/SwapPage/index.tsx
+++ webapp/src/containers/SwapPage/index.tsx
@@ -54,7 +54,7 @@ import PersistentStore from '../../utils/persistentStore';
 import Header from '../HeaderComponent';
 import openNewTab from '../../utils/openNewTab';
 import NumberMask from '../../components/NumberMask';
-import ViewOnChain from 'src/components/ViewOnChain';
+import ViewOnChain from '../../../src/components/ViewOnChain';
 import { PaymentRequestModel } from '../WalletPage/components/ReceivePage/PaymentRequestList';
 
 interface SwapPageProps {
