30262a1bf24bcccff98d1cf8d682f8dc536a541c
JJ Adonis <josejaimecareers@gmail.com>
2021-01-22 15:51:24 +0800

    feat: listunspent big number

--- webapp/src/constants/rpcModel.ts
+++ webapp/src/constants/rpcModel.ts
@@ -72,3 +72,19 @@ export interface PeerInfoModel {
   bytessent_per_msg: BytesSentModel;
   bytesrecv_per_msg: BytesReceiveModel;
 }
+
+export interface ListUnspentModel {
+  txid: string;
+  vout: number;
+  address: string;
+  amount: number;
+  confirmations: number;
+  label?: string;
+  scriptPubKey?: string;
+  redeemScript?: string;
+  witnessScript?: string;
+  spendable?: boolean;
+  solvable?: boolean;
+  desc?: string;
+  safe?: boolean;
+}
