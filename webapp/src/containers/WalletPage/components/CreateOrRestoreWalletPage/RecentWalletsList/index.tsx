9a03055221a59ab6080201b9c8fb035f895d1002
JJ Adonis <josejaimecareers@gmail.com>
2021-02-05 16:51:30 +0800

    feat: added translations

--- webapp/src/containers/WalletPage/components/CreateOrRestoreWalletPage/RecentWalletsList/index.tsx
+++ webapp/src/containers/WalletPage/components/CreateOrRestoreWalletPage/RecentWalletsList/index.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { I18n } from 'react-redux-i18n';
 import styles from './RecentWalletsList.module.scss';
 import { Button, ListGroup, ListGroupItem } from 'reactstrap';
-import CopyToClipboard from '../../../../../components/CopyToClipboard';
 import { shortenedPathAddress } from '../../../../../utils/utility';
 
 interface RecentWalletsListProps {
   paths: string[];
   onRestore: (p: string) => void;
 }
-
-const handleCopy = () => {
-  console.log('Copied!');
-};
 
 const RecentWalletsList: React.FunctionComponent<RecentWalletsListProps> = (
   props: RecentWalletsListProps
@@ -25,7 +20,6 @@ const RecentWalletsList: React.FunctionComponent<RecentWalletsListProps> = (
           <ListGroupItem key={index} className={styles.listGroupItem}>
             <div>
               <span>{shortenedPathAddress(p)}</span>
-              <CopyToClipboard value={p} handleCopy={handleCopy} />
             </div>
             <div>
               <Button color='link' className='p-0' onClick={() => onRestore(p)}>
