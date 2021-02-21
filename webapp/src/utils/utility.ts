7060b3baf5825775e5dc8ae397c5046aa1be7e94
JJ Adonis <josejaimecareers@gmail.com>
2021-02-21 19:04:32 +0800

    sample

--- webapp/src/utils/utility.ts
+++ webapp/src/utils/utility.ts
@@ -1497,14 +1497,14 @@ export const shortenedPathAddress = (p: string): string => {
 };
 
 export const getFormattedTime = () => {
-  const today = new Date();
-  const y = today.getFullYear();
-  const m = today.getMonth() + 1;
-  const d = today.getDate();
-  const h = today.getHours();
-  const mi = today.getMinutes();
-  const s = today.getSeconds();
-  return `${y}-${m}-${d}_${h}-${mi}-${s}`;
+const today = new Date();
+const y = today.getFullYear();
+const m = today.getMonth() + 1;
+const d = today.getDate();
+const h = today.getHours();
+const mi = today.getMinutes();
+const s = today.getSeconds();
+return `${y}-${m}-${d}_${h}-${mi}-${s}`;
 };
 
 export const checkRPCErrorMessagePending = (message: string): string => {
