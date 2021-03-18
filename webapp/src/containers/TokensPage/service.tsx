aa65191b378d301f2ffe7514affa5484c585a3f0
Harsh <fullstackninja864@gmail.com>
2021-03-18 13:46:13 +0530

    fix token search bar

--- webapp/src/containers/TokensPage/service.tsx
+++ webapp/src/containers/TokensPage/service.tsx
@@ -1,10 +1,8 @@
 import RpcClient from '../../utils/rpc-client';
 import isEmpty from 'lodash/isEmpty';
 import {
-  DEFAULT_DFI_FOR_ACCOUNT_TO_ACCOUNT,
   DFI_SYMBOL,
   LIST_TOKEN_PAGE_SIZE,
-  MINIMUM_DFI_AMOUNT_FOR_MASTERNODE,
   MINIMUM_DFI_REQUIRED_FOR_TOKEN_CREATION,
   UNDEFINED_STRING,
 } from '../../constants';
@@ -61,7 +59,7 @@ export const getTransactionInfo = async (txId): Promise<any> => {
 
 export const handleFetchTokens = async () => {
   const rpcClient = new RpcClient();
-  return await fetchTokenDataWithPagination(
+  return fetchTokenDataWithPagination(
     0,
     LIST_TOKEN_PAGE_SIZE,
     rpcClient.listTokens
