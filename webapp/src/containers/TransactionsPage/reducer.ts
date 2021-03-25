99461917c08a44c5e4b37e3b649f5ec7f45fb52d
Saurabh Shinde <saurabhshinde391@gmail.com>
2021-03-26 00:47:22 +0530

    Implements reducer saga service types and transaction page

---
+++ webapp/src/containers/TransactionsPage/reducer.ts
@@ -0,0 +1,370 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { defaultWalletMap, TransactionState } from './types';
+
+export const initialState: TransactionState = {
+  accountTokens: [],
+  isAccountTokensLoaded: false,
+  isAccountLoadingTokens: false,
+  accountHistoryCount: '',
+  accountHistoryCountLoaded: false,
+  accountHistoryCountLoading: false,
+  minBlockHeight: 0,
+  maxBlockData: {},
+  tokens: [],
+  isTokensLoaded: false,
+  isLoadingTokens: false,
+  walletBalance: 0,
+  isBalanceFetching: false,
+  isBalanceError: '',
+  utxoDfi: 0,
+  isUtxoDfiFetching: false,
+  isUtxoDfiError: '',
+  pendingBalance: 0,
+  isPendingBalanceFetching: false,
+  isPendingBalanceError: '',
+  paymentRequests: [],
+  walletTxns: [],
+  walletTxnCount: 0,
+  isWalletTxnsLoading: false,
+  totalFetchedTxns: [],
+  walletPageCounter: 1,
+  stopPagination: false,
+  blockChainInfo: {},
+  receivedData: {
+    amountToReceive: '',
+    amountToReceiveDisplayed: 0,
+    receiveMessage: '',
+    showBackdrop: '',
+    receiveStep: 'default',
+  },
+  sendData: {
+    walletBalance: 0,
+    amountToSend: '',
+    amountToSendDisplayed: 0,
+    toAddress: '',
+    scannerOpen: false,
+    flashed: '',
+    showBackdrop: '',
+    sendStep: 'default',
+    waitToSend: 5,
+  },
+  isWalletCreating: false,
+  isErrorCreatingWallet: '',
+  isWalletRestoring: false,
+  isErrorRestoringWallet: '',
+  isWalletCreatedFlag: false,
+  listAccountHistoryData: {
+    isLoading: false,
+    isError: '',
+    data: [],
+    stop: false,
+  },
+  combineAccountHistoryData: {
+    isLoading: false,
+    isError: '',
+    data: [],
+  },
+  restartCriteria: {
+    isLoading: false,
+    data: true,
+    isError: '',
+  },
+  walletMap: { ...defaultWalletMap },
+  walletMapError: '',
+  isWalletEncrypted: false,
+  isErrorUnlockWallet: '',
+  isWalletUnlocked: false,
+  lockedUntil: 0,
+};
+const configSlice = createSlice({
+  name: 'transaction',
+  initialState,
+  reducers: {
+    fetchAccountTokensRequest(state) {
+      state.isAccountLoadingTokens = true;
+    },
+    fetchAccountTokensSuccess(state, action) {
+      state.accountTokens = action.payload.accountTokens;
+      state.isAccountLoadingTokens = false;
+      state.isAccountTokensLoaded = true;
+    },
+    fetchAccountTokensFailure(state, action) {
+      state.accountTokens = [];
+      state.isAccountLoadingTokens = false;
+      state.isAccountTokensLoaded = true;
+    },
+    accountHistoryCountRequest(state, action) {
+      state.accountHistoryCount = '';
+    },
+    accountHistoryCountSuccess(state, action) {
+      state.accountHistoryCount = action.payload.accountHistoryCount;
+      state.accountHistoryCountLoading = false;
+      state.accountHistoryCountLoaded = true;
+    },
+    accountHistoryCountFailure(state, action) {
+      state.accountTokens = [];
+      state.accountHistoryCountLoading = false;
+      state.accountHistoryCountLoaded = true;
+    },
+    fetchTokensRequest(state) {
+      state.isLoadingTokens = true;
+    },
+    fetchTokensSuccess(state, action) {
+      state.tokens = action.payload.tokens;
+      state.isLoadingTokens = false;
+      state.isTokensLoaded = true;
+    },
+    fetchTokensFailure(state, action) {
+      state.tokens = [];
+      state.isLoadingTokens = false;
+      state.isTokensLoaded = true;
+    },
+    fetchPaymentRequest(state) {
+      state.paymentRequests = [];
+    },
+    fetchPaymentRequestsSuccess(state, action) {
+      state.paymentRequests = action.payload;
+    },
+    fetchPaymentRequestsFailure(state, action) {
+      state.paymentRequests = [];
+    },
+    fetchWalletTxnsRequest(state, action) {
+      state.isWalletTxnsLoading = true;
+    },
+    fetchWalletTxnsSuccess(state, action) {
+      state.walletTxns = action.payload.walletTxns;
+      state.totalFetchedTxns = action.payload.totalFetchedTxns;
+      state.walletTxnCount = action.payload.walletTxnCount;
+      state.walletPageCounter = action.payload.walletPageCounter;
+      state.isWalletTxnsLoading = false;
+      state.stopPagination = false;
+    },
+    fetchWalletTxnsFailure(state, action) {
+      state.walletTxns = [];
+      state.isWalletTxnsLoading = false;
+    },
+    removeReceiveTxnsRequest(state, action) {},
+    removeReceiveTxnsSuccess(state, action) {
+      state.paymentRequests = action.payload;
+    },
+    removeReceiveTxnsFailure(state, action) {},
+    addReceiveTxnsRequest(state, action) {},
+    addReceiveTxnsSuccess(state, action) {
+      state.paymentRequests = action.payload;
+    },
+    addReceiveTxnsFailure(state, action) {},
+    fetchSendDataRequest(state) {},
+
+    fetchWalletBalanceRequest(state) {
+      state.isBalanceFetching = true;
+      state.isBalanceError = '';
+    },
+    fetchWalletBalanceSuccess(state, action) {
+      state.walletBalance = action.payload;
+      state.isBalanceFetching = false;
+    },
+    fetchWalletBalanceFailure(state, action) {
+      state.walletBalance = 0;
+      state.isBalanceFetching = false;
+      state.isBalanceError = action.payload;
+    },
+    fetchPendingBalanceSuccess(state, action) {
+      state.pendingBalance = action.payload;
+      state.isPendingBalanceFetching = false;
+    },
+    fetchPendingBalanceFailure(state, action) {
+      state.walletBalance = 0;
+      state.isPendingBalanceFetching = false;
+      state.isPendingBalanceError = action.payload;
+    },
+    stopWalletTxnPagination(state) {
+      state.stopPagination = true;
+    },
+    setBlockChainInfo(state, action) {
+      state.blockChainInfo = action.payload;
+    },
+    resetCreateWalletError(state, action) {
+      state.isWalletCreating = false;
+      state.isErrorCreatingWallet = '';
+    },
+    resetRestoreWalletError(state, action) {
+      state.isWalletRestoring = false;
+      state.isErrorRestoringWallet = '';
+    },
+    fetchInstantBalanceRequest(state) {},
+    setIsWalletCreatedRequest(state, action) {
+      state.isWalletCreatedFlag = action.payload;
+    },
+    fetchWalletTokenTransactionsListRequestLoading(state, action) {
+      state.listAccountHistoryData.isLoading = true;
+      state.listAccountHistoryData.isError = '';
+      state.listAccountHistoryData.data = [];
+    },
+    fetchWalletTokenTransactionsListRequestSuccess(state, action) {
+      state.listAccountHistoryData.isLoading = false;
+      state.listAccountHistoryData.isError = '';
+      state.listAccountHistoryData.data = action.payload.data;
+      state.minBlockHeight = action.payload.minBlockHeight;
+      state.maxBlockData = action.payload.maxBlockData;
+    },
+    fetchWalletTokenTransactionsListRequestFailure(state, action) {
+      state.listAccountHistoryData.isLoading = false;
+      state.listAccountHistoryData.isError = action.payload;
+      state.listAccountHistoryData.data = [];
+    },
+    fetchWalletTokenTransactionsListRequestStop(state) {
+      state.listAccountHistoryData.isLoading = false;
+      state.listAccountHistoryData.stop = true;
+    },
+    checkRestartCriteriaRequestLoading(state) {
+      state.restartCriteria.isLoading = true;
+      state.restartCriteria.data = true;
+      state.restartCriteria.isError = '';
+    },
+    checkRestartCriteriaRequestSuccess(state, action) {
+      state.restartCriteria.isLoading = false;
+      state.restartCriteria.data = action.payload;
+      state.restartCriteria.isError = '';
+    },
+    checkRestartCriteriaRequestFailure(state, action) {
+      state.restartCriteria.isLoading = false;
+      state.restartCriteria.data = true;
+      state.restartCriteria.isError = action.payload;
+    },
+    fetchBlockDataForTrxRequestLoading(state, action) {
+      state.combineAccountHistoryData.isLoading = true;
+      state.combineAccountHistoryData.data = [];
+      state.combineAccountHistoryData.isError = action.payload;
+    },
+    fetchBlockDataForTrxRequestSuccess(state, action) {
+      state.combineAccountHistoryData.isLoading = false;
+      state.combineAccountHistoryData.data = action.payload;
+      state.combineAccountHistoryData.isError = '';
+    },
+    fetchBlockDataForTrxRequestFailure(state, action) {
+      state.combineAccountHistoryData.isLoading = false;
+      state.combineAccountHistoryData.data = [];
+      state.combineAccountHistoryData.isError = action.payload;
+    },
+    fetchWalletTokenTransactionsListResetRequest(state) {
+      state.listAccountHistoryData.isLoading = false;
+      state.listAccountHistoryData.isError = '';
+      state.listAccountHistoryData.data = [];
+    },
+    fetchWalletReset(state) {
+      state.walletBalance = 0;
+      state.accountTokens = [];
+      state.isAccountLoadingTokens = false;
+      state.isAccountTokensLoaded = false;
+      state.tokens = [];
+      state.isTokensLoaded = false;
+      state.isLoadingTokens = false;
+    },
+    fetchWalletMapRequest(state) {
+      state.walletMap = { ...defaultWalletMap };
+      state.walletMapError = '';
+    },
+    fetchWalletMapSuccess(state, action) {
+      state.walletMap = action.payload;
+      state.walletMapError = '';
+    },
+    fetchWalletMapFailure(state, action) {
+      state.walletMapError = action.payload;
+      state.walletMap = { ...defaultWalletMap };
+    },
+    restoreWalletViaBackupFailure(state, action) {
+      state.isWalletRestoring = false;
+      state.isErrorRestoringWallet = action.payload;
+    },
+    startRestoreWalletViaRecent(state, action) {
+      state.isWalletRestoring = true;
+      state.isErrorRestoringWallet = '';
+    },
+    startBackupWalletViaExitModal(state) {},
+    unlockWalletStart(state, action) {
+      state.isErrorUnlockWallet = '';
+    },
+    unlockWalletSuccess(state, action) {
+      state.isWalletUnlocked = action.payload;
+    },
+    unlockWalletFailure(state, action) {
+      state.isWalletUnlocked = false;
+      state.isErrorUnlockWallet = action.payload;
+    },
+    lockWalletStart(state) {
+      state.isWalletUnlocked = false;
+    },
+    enableAutoLockStart(state) {
+      state.isWalletUnlocked = false;
+      state.lockedUntil = 0;
+    },
+    setLockedUntil(state, action) {
+      state.isWalletUnlocked = true;
+      state.lockedUntil = action.payload;
+    },
+  },
+});
+
+const { actions, reducer } = configSlice;
+
+export const {
+  fetchAccountTokensRequest,
+  fetchAccountTokensSuccess,
+  fetchAccountTokensFailure,
+  accountHistoryCountRequest,
+  accountHistoryCountSuccess,
+  accountHistoryCountFailure,
+  fetchTokensRequest,
+  fetchTokensSuccess,
+  fetchTokensFailure,
+  fetchPaymentRequest,
+  fetchPaymentRequestsSuccess,
+  fetchPaymentRequestsFailure,
+  fetchWalletTxnsRequest,
+  fetchWalletTxnsSuccess,
+  fetchWalletTxnsFailure,
+  removeReceiveTxnsRequest,
+  removeReceiveTxnsSuccess,
+  removeReceiveTxnsFailure,
+  addReceiveTxnsRequest,
+  addReceiveTxnsSuccess,
+  addReceiveTxnsFailure,
+  fetchSendDataRequest,
+  fetchWalletBalanceRequest,
+  fetchWalletBalanceSuccess,
+  fetchWalletBalanceFailure,
+  fetchPendingBalanceSuccess,
+  fetchPendingBalanceFailure,
+  stopWalletTxnPagination,
+  setBlockChainInfo,
+  resetCreateWalletError,
+  resetRestoreWalletError,
+  fetchInstantBalanceRequest,
+  setIsWalletCreatedRequest,
+  fetchWalletTokenTransactionsListRequestLoading,
+  fetchWalletTokenTransactionsListRequestSuccess,
+  fetchWalletTokenTransactionsListRequestFailure,
+  checkRestartCriteriaRequestLoading,
+  checkRestartCriteriaRequestSuccess,
+  checkRestartCriteriaRequestFailure,
+  fetchWalletTokenTransactionsListRequestStop,
+  fetchWalletTokenTransactionsListResetRequest,
+  fetchBlockDataForTrxRequestLoading,
+  fetchBlockDataForTrxRequestSuccess,
+  fetchBlockDataForTrxRequestFailure,
+  fetchWalletReset,
+  fetchWalletMapRequest,
+  fetchWalletMapSuccess,
+  fetchWalletMapFailure,
+  restoreWalletViaBackupFailure,
+  startRestoreWalletViaRecent,
+
+  lockWalletStart,
+  enableAutoLockStart,
+  unlockWalletStart,
+  unlockWalletSuccess,
+  unlockWalletFailure,
+  setLockedUntil,
+} = actions;
+
+export default reducer;
---
+++ webapp/src/containers/TransactionsPage/saga.tsx
@@ -0,0 +1,453 @@
+import { call, put, takeLatest, select, all, delay } from 'redux-saga/effects';
+import * as log from '../../utils/electronLogger';
+import {
+  fetchTokensSuccess,
+  fetchTokensRequest,
+  fetchTokensFailure,
+  fetchPaymentRequest,
+  fetchPaymentRequestsSuccess,
+  fetchPaymentRequestsFailure,
+  fetchWalletTxnsRequest,
+  fetchWalletTxnsSuccess,
+  fetchWalletTxnsFailure,
+  addReceiveTxnsRequest,
+  addReceiveTxnsSuccess,
+  addReceiveTxnsFailure,
+  fetchWalletBalanceSuccess,
+  fetchWalletBalanceFailure,
+  removeReceiveTxnsRequest,
+  removeReceiveTxnsSuccess,
+  removeReceiveTxnsFailure,
+  fetchPendingBalanceSuccess,
+  fetchPendingBalanceFailure,
+  fetchAccountTokensRequest,
+  fetchAccountTokensSuccess,
+  fetchAccountTokensFailure,
+  stopWalletTxnPagination,
+  setBlockChainInfo,
+  fetchInstantBalanceRequest,
+  setIsWalletCreatedRequest,
+  fetchWalletTokenTransactionsListRequestLoading,
+  fetchWalletTokenTransactionsListRequestSuccess,
+  fetchWalletTokenTransactionsListRequestFailure,
+  checkRestartCriteriaRequestLoading,
+  checkRestartCriteriaRequestSuccess,
+  checkRestartCriteriaRequestFailure,
+  fetchBlockDataForTrxRequestLoading,
+  fetchBlockDataForTrxRequestSuccess,
+  fetchBlockDataForTrxRequestFailure,
+  accountHistoryCountRequest,
+  accountHistoryCountSuccess,
+  accountHistoryCountFailure,
+  fetchWalletMapRequest,
+  fetchWalletMapSuccess,
+  fetchWalletMapFailure,
+} from './reducer';
+import {
+  handleFetchTokens,
+  handleGetPaymentRequest,
+  handelAddReceiveTxns,
+  handelFetchWalletTxns,
+  handleFetchWalletBalance,
+  handelRemoveReceiveTxns,
+  handleFetchPendingBalance,
+  handleBlockData,
+  getAddressInfo,
+  getBlockChainInfo,
+  handleFetchAccounts,
+  getListAccountHistory,
+  handleRestartCriteria,
+  handleFetchAccountHistoryCount,
+  startBackupViaExitModal,
+} from './service';
+import store from '../../app/rootStore';
+import showNotification from '../../utils/notifications';
+import {
+  convertEpochToDate,
+  getErrorMessage,
+  getNetworkType,
+  hdWalletCheck,
+} from '../../utils/utility';
+import { paginate, queuePush } from '../../utils/utility';
+import { I18n } from 'react-redux-i18n';
+import uniqBy from 'lodash/uniqBy';
+import cloneDeep from 'lodash/cloneDeep';
+import isEmpty from 'lodash/isEmpty';
+import { AMOUNT_SEPARATOR, MAX_WALLET_TXN_PAGE_SIZE } from '../../constants';
+import minBy from 'lodash/minBy';
+import orderBy from 'lodash/orderBy';
+import { uid } from 'uid';
+import { checkWalletEncryption, getWalletMap } from '../../app/service';
+import {
+  openEncryptWalletModal,
+  openExitWalletModal,
+  startResetWalletDatRequest,
+} from '../PopOver/reducer';
+import { setDefaultLockTimeout } from '../SettingsPage/reducer';
+import { WalletMap } from '@defi_types/walletMap';
+import { TimeoutLockEnum } from '../SettingsPage/types';
+
+export function* getNetwork() {
+  const {
+    blockChainInfo: { chain },
+  } = yield select((state) => state.transaction);
+  return chain;
+}
+
+function* getPaymentRequestState() {
+  const { paymentRequests = [] } = yield select((state) => state.wallet);
+  return cloneDeep(paymentRequests);
+}
+
+export async function addHdSeedCheck(list) {
+  const result = list.map(async (data) => {
+    return {
+      ...data,
+      hdSeed: await hdWalletCheck(data.address),
+    };
+  });
+  const resolvedData = await Promise.all(result);
+  return resolvedData;
+}
+
+export function* addReceiveTxns(action: any) {
+  try {
+    const cloneDeepPaymentRequests = yield call(getPaymentRequestState);
+
+    const networkName = yield call(getNetwork);
+
+    yield call(handelAddReceiveTxns, action.payload, networkName);
+
+    cloneDeepPaymentRequests.push(action.payload);
+
+    yield put(addReceiveTxnsSuccess(cloneDeepPaymentRequests));
+  } catch (e) {
+    showNotification(I18n.t('alerts.addReceiveTxnsFailure'), e.message);
+    yield put(addReceiveTxnsFailure(e.message));
+    log.error(e);
+  }
+}
+
+export function* removeReceiveTxns(action: any) {
+  try {
+    const cloneDeepPaymentRequests = yield call(getPaymentRequestState);
+
+    const networkName = yield call(getNetwork);
+
+    yield call(handelRemoveReceiveTxns, action.payload, networkName);
+
+    const result = cloneDeepPaymentRequests.filter(
+      (ele) => ele.id && ele.id.toString() !== action.payload.toString()
+    );
+
+    yield put(removeReceiveTxnsSuccess(result));
+  } catch (e) {
+    showNotification(I18n.t('alerts.removeReceiveTxnsFailure'), e.message);
+    yield put(removeReceiveTxnsFailure(e.message));
+    log.error(e);
+  }
+}
+
+export function* fetchPayments() {
+  try {
+    const networkName = yield call(getNetwork);
+    const data = yield call(handleGetPaymentRequest, networkName);
+    const list = yield all(
+      data.map((item) => {
+        item.id = item.id ?? uid();
+        return call(getAddressInfo, item.address);
+      })
+    );
+    const result = data.filter((item) => {
+      const found = list.find(
+        (ele) => ele.address === item.address && ele.ismine && !ele.iswatchonly
+      );
+      return !isEmpty(found);
+    });
+    const finalResult = yield call(addHdSeedCheck, result);
+    yield put(fetchPaymentRequestsSuccess(finalResult));
+  } catch (e) {
+    showNotification(I18n.t('alerts.paymentRequestsFailure'), e.message);
+    yield put({ type: fetchPaymentRequestsFailure.type, payload: e.message });
+    log.error(e);
+  }
+}
+
+export function* fetchWalletTxns(action) {
+  const { currentPage: pageNo, pageSize, intialLoad } = action.payload;
+  const { totalFetchedTxns, walletTxnCount, walletPageCounter } = yield select(
+    (state) => state.transaction
+  );
+  const callBack = (err, result) => {
+    if (err) {
+      store.dispatch(fetchWalletTxnsFailure(err.message));
+      log.error(err);
+      return;
+    }
+    if (result && result.walletTxns) {
+      const distinct = uniqBy(result.walletTxns, 'txnId');
+      const previousFetchedTxns = intialLoad ? [] : totalFetchedTxns;
+      const totalFetched = previousFetchedTxns.concat(distinct);
+      store.dispatch(
+        fetchWalletTxnsSuccess({
+          walletTxnCount: result.walletTxnCount,
+          totalFetchedTxns: totalFetched,
+          walletTxns: paginate(totalFetched, pageSize, pageNo),
+          walletPageCounter: intialLoad ? 1 : walletPageCounter + 1,
+        })
+      );
+    } else {
+      const noDataFound = 'No data found';
+      showNotification(I18n.t('alerts.walletTxnsFailure'), noDataFound);
+      store.dispatch(fetchWalletTxnsFailure(noDataFound));
+      log.error(`${noDataFound}`, 'fetchWalletTxns');
+    }
+  };
+  if (totalFetchedTxns.length <= (pageNo - 1) * pageSize || intialLoad) {
+    yield put(stopWalletTxnPagination());
+    queuePush(
+      handelFetchWalletTxns,
+      [walletPageCounter, MAX_WALLET_TXN_PAGE_SIZE],
+      callBack
+    );
+  } else {
+    yield put(
+      fetchWalletTxnsSuccess({
+        totalFetchedTxns,
+        walletTxnCount,
+        walletTxns: paginate(totalFetchedTxns, pageSize, pageNo),
+        walletPageCounter,
+      })
+    );
+  }
+}
+
+//* If wallet is existing on conf, set wallet loaded
+export function* setWalletExistingIfInConf(conf: any) {
+  const network = getNetworkType();
+  const isWalletCreatedConf =
+    conf != null &&
+    conf[network] != null &&
+    conf[network]?.wallet != null &&
+    conf[network]?.walletdir != null;
+  store.dispatch(setIsWalletCreatedRequest(isWalletCreatedConf));
+}
+
+export function* fetchTokens() {
+  try {
+    const data = yield call(handleFetchTokens);
+    yield put({
+      type: fetchTokensSuccess.type,
+      payload: { tokens: data },
+    });
+  } catch (e) {
+    yield put({ type: fetchTokensFailure.type, payload: e.message });
+    log.error(e);
+  }
+}
+
+export function* accountHistoryCount(action) {
+  const {
+    payload: { no_rewards, token },
+  } = action;
+
+  try {
+    const data = yield call(handleFetchAccountHistoryCount, no_rewards, token);
+    yield put({
+      type: accountHistoryCountSuccess.type,
+      payload: { accountHistoryCount: data },
+    });
+  } catch (e) {
+    yield put({
+      type: accountHistoryCountFailure.type,
+      payload: getErrorMessage(e),
+    });
+    log.error(e);
+  }
+}
+
+export function* fetchAccountTokens() {
+  try {
+    const data = yield call(handleFetchAccounts);
+    yield put({
+      type: fetchAccountTokensSuccess.type,
+      payload: { accountTokens: data },
+    });
+  } catch (e) {
+    yield put({
+      type: fetchAccountTokensFailure.type,
+      payload: getErrorMessage(e),
+    });
+    log.error(e);
+  }
+}
+
+export function* fetchInstantBalance() {
+  try {
+    const result = yield call(handleFetchWalletBalance);
+    yield put(fetchWalletBalanceSuccess(result));
+  } catch (err) {
+    yield put(fetchWalletBalanceFailure(err.message));
+    log.error(err);
+  }
+}
+
+export function* fetchInstantPendingBalance() {
+  try {
+    const result = yield call(handleFetchPendingBalance);
+    yield put(fetchPendingBalanceSuccess(result));
+  } catch (err) {
+    yield put(fetchPendingBalanceFailure(err.message));
+    log.error(err);
+  }
+}
+
+export function* fetchWalletTokenTransactionsList(action) {
+  try {
+    const {
+      symbol,
+      limit,
+      includeRewards,
+      pageNum,
+      minBlockHeight,
+      cancelToken,
+    } = action.payload;
+
+    let blockHeight = minBlockHeight;
+    const tempData = {};
+    const { maxBlockData } = yield select((state) => state.transaction);
+    const currentData = maxBlockData[symbol];
+
+    if (pageNum === 1 || !currentData) {
+      tempData[symbol] = [];
+    } else if (
+      currentData.length > 0 &&
+      pageNum < currentData[currentData.length - 1].page
+    ) {
+      blockHeight = currentData[currentData.length - 2]?.maxBlockHeight;
+      tempData[symbol] = currentData.filter(
+        (val, index) => index < currentData.length - 1
+      );
+    } else {
+      const currentBlockData = {
+        page: pageNum,
+        token: symbol,
+        maxBlockHeight: minBlockHeight,
+      };
+      tempData[symbol] = [...currentData, currentBlockData];
+    }
+
+    const data: any[] = yield call(getListAccountHistory, {
+      limit,
+      token: symbol,
+      no_rewards: !includeRewards,
+      cancelToken: cancelToken,
+      blockHeight,
+    });
+
+    const minHeightData = data.length ? minBy(data, 'blockHeight') : -1;
+    const minBlockHeightData = minHeightData.blockHeight - 1;
+
+    const parsedData = data.map((d) => {
+      return {
+        owner: d.owner,
+        blockHeight: d.blockHeight,
+        blockHash: d.blockHash,
+        blockTime: convertEpochToDate(d.blockTime),
+        type: d.type,
+        txn: d.txn,
+        txid: d.txid,
+        amountData: d.amounts.map((amount) => {
+          return {
+            unit: amount.split(AMOUNT_SEPARATOR)[1],
+            amount: amount.split(AMOUNT_SEPARATOR)[0],
+          };
+        }),
+      };
+    });
+
+    yield put(
+      fetchWalletTokenTransactionsListRequestSuccess({
+        data: orderBy(parsedData, 'blockHeight', 'desc'),
+        minBlockHeight: minBlockHeightData,
+        maxBlockData: tempData,
+      })
+    );
+  } catch (err) {
+    log.error(err, 'fetchWalletTokenTransactionsList');
+    yield put(fetchWalletTokenTransactionsListRequestFailure(err.message));
+  }
+}
+
+export function* getBlockData(item) {
+  const blockData = yield call(handleBlockData, item.blockHeight);
+  return {
+    ...item,
+    blockData,
+  };
+}
+
+export function* fetchBlockDataForTrx(action) {
+  try {
+    const trxArray: any[] = action.payload;
+    const updated = yield all(trxArray.map((item) => call(getBlockData, item)));
+    yield put(fetchBlockDataForTrxRequestSuccess(updated));
+  } catch (err) {
+    log.error(err, 'fetchBlockDataForTrx');
+    yield put(fetchBlockDataForTrxRequestFailure(err.message));
+  }
+}
+
+export function* checkRestartCriteria() {
+  try {
+    const restartCriteria = yield call(handleRestartCriteria);
+    yield put(checkRestartCriteriaRequestSuccess(restartCriteria));
+  } catch (err) {
+    log.error(err, 'checkRestartCriteria');
+    yield put(checkRestartCriteriaRequestFailure(err.message));
+  }
+}
+
+export function* fetchWalletMap() {
+  try {
+    const walletMap: WalletMap = yield call(getWalletMap);
+    if (walletMap) {
+      yield put(fetchWalletMapSuccess(walletMap));
+      yield put(
+        setDefaultLockTimeout(
+          walletMap.lockTimeout || TimeoutLockEnum.FIVE_MINUTES
+        )
+      );
+    }
+  } catch (err) {
+    log.error(err, 'fetchWalletMap');
+    yield put(fetchWalletMapFailure(err?.message));
+  }
+}
+
+function* mySaga() {
+  yield takeLatest(addReceiveTxnsRequest.type, addReceiveTxns);
+  yield takeLatest(removeReceiveTxnsRequest.type, removeReceiveTxns);
+  yield takeLatest(fetchPaymentRequest.type, fetchPayments);
+  yield takeLatest(fetchWalletTxnsRequest.type, fetchWalletTxns);
+  yield takeLatest(fetchTokensRequest.type, fetchTokens);
+  yield takeLatest(fetchAccountTokensRequest.type, fetchAccountTokens);
+  yield takeLatest(accountHistoryCountRequest.type, accountHistoryCount);
+  yield takeLatest(fetchInstantBalanceRequest.type, fetchInstantBalance);
+
+  yield takeLatest(
+    fetchWalletTokenTransactionsListRequestLoading.type,
+    fetchWalletTokenTransactionsList
+  );
+  yield takeLatest(
+    checkRestartCriteriaRequestLoading.type,
+    checkRestartCriteria
+  );
+  yield takeLatest(
+    fetchBlockDataForTrxRequestLoading.type,
+    fetchBlockDataForTrx
+  );
+  yield takeLatest(fetchWalletMapRequest.type, fetchWalletMap);
+}
+
+export default mySaga;
