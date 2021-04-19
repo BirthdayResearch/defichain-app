c6f4a43abaf843835fd53fae255fb40602b388d0
thedoublejay <josejaimecareers@gmail.com>
2021-04-19 11:02:44 +0800

    feat: tooltip

--- webapp/src/containers/WalletPage/components/Balances/index.tsx
+++ webapp/src/containers/WalletPage/components/Balances/index.tsx
@@ -25,6 +25,7 @@ import CreateOrRestoreWalletPage from '../CreateOrRestoreWalletPage';
 import BalancesTokenCard from './BalancesTokenCard';
 import styles from './Balances.module.scss';
 import classnames from 'classnames';
+import { MdInfo } from 'react-icons/md';
 
 export type BalanceToken = Partial<IToken>;
 
@@ -150,8 +151,13 @@ const BalancesPage: React.FunctionComponent = () => {
             <div className='btcCard mb-5'>
               <BalancesTokenCard token={btcNative as IToken} size={largeIcon} />
             </div>
-            <h2>
+            <h2 className='d-flex align-items-center'>
               <span>{I18n.t('containers.wallet.walletPage.tokens')}</span>
+              <MdInfo
+                className={`ml-1 ${styles.icons} cursorPointer`}
+                id='token_item'
+                size={23}
+              />
             </h2>
             <div className={classnames({ cardTable: true })}>
               {tokens.map((token, index) => (
