6709ae05f75d90ce80a00fdd182159350026622a
thedoublejay <josejaimecareers@gmail.com>
2021-05-12 14:06:38 +0800

    chore: bump snapshot 840339

--- typings/snapshot.ts
+++ typings/snapshot.ts
@@ -2,7 +2,7 @@ export const SNAPSHOT_EU =
   'https://defi-snapshots-europe.s3.eu-central-1.amazonaws.com/';
 export const SNAPSHOT_ASIA =
   'https://defi-snapshots.s3-ap-southeast-1.amazonaws.com/';
-export const SNAPSHOT_BLOCK = '838093';
+export const SNAPSHOT_BLOCK = '840339';
 export const SNAPSHOT_FILENAME = `snapshot-mainnet-${SNAPSHOT_BLOCK}.zip`;
 export const OFFICIAL_SNAPSHOT_URL = `${SNAPSHOT_EU}${SNAPSHOT_FILENAME}`;
 export const SNAPSHOT_PROVIDER = 'DeFiChain Foundation';
