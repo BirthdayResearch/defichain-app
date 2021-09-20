663d81b0742c6ba2f1cb7af2cb0b72fab6a23ab0
thedoublejay <josejaimecareers@gmail.com>
2021-09-20 17:50:54 +0800

    updated web server

--- webapp/webpack/config/devServer.js
+++ webapp/webpack/config/devServer.js
@@ -1,21 +1,13 @@
 export const defaultPort = 5000;
 
 export const devServerConfig = {
-  publicPath: '/',
   port: defaultPort,
   compress: true,
-  noInfo: false,
-  stats: 'errors-only',
-  inline: true,
-  lazy: false,
   hot: true,
-  overlay: false,
-  headers: { 'Access-Control-Allow-Origin': '*' },
-  watchOptions: {
-    aggregateTimeout: 300,
-    ignored: /node_modules/,
-    poll: 1000,
+  client: {
+    overlay: false,
   },
+  headers: { 'Access-Control-Allow-Origin': '*' },
   historyApiFallback: {
     disableDotRule: true,
   },
--- webapp/webpack/webpack.config.base.js
+++ webapp/webpack/webpack.config.base.js
@@ -51,4 +51,10 @@ export default {
       process: require.resolve('process/browser'),
     },
   },
+  watchOptions: {
+    aggregateTimeout: 300,
+    ignored: /node_modules/,
+    poll: 1000,
+  },
+  stats: 'errors-only',
 };
