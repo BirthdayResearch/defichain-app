6e61e8128f2aa9bc61e24b6e9bbff3b1166eef15
JJ Adonis <josejaimecareers@gmail.com>
2022-04-01 11:45:14 +0800

    bump node and display poolshares

--- webapp/src/containers/LiquidityPage/components/LiquidityList/index.tsx
+++ webapp/src/containers/LiquidityPage/components/LiquidityList/index.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { I18n } from 'react-redux-i18n';
 import { Row, Col } from 'reactstrap';
-import cloneDeep from 'lodash/cloneDeep';
 
 import LiquidityAccordion from '../../../../components/LiquidityAccordion';
-import { TOKEN_LIST_PAGE_SIZE } from '../../../../constants';
 
 interface LiquidityListProps {
   poolshares: any;
@@ -14,32 +12,13 @@ interface LiquidityListProps {
 const LiquidityList: React.FunctionComponent<LiquidityListProps> = (
   props: LiquidityListProps
 ) => {
-  const defaultPage = 1;
-  const [tableData, settableData] = useState<any>([]);
-  const [currentPage, setCurrentPage] = useState<number>(defaultPage);
   const { poolshares } = props;
-  const pageSize = TOKEN_LIST_PAGE_SIZE;
-
-  function paginate(pageNumber, poolsharesList?: any[]) {
-    const clone = cloneDeep(poolsharesList || poolshares);
-    const tableData = clone.slice(
-      (pageNumber - 1) * pageSize,
-      pageNumber * pageSize
-    );
-    setCurrentPage(pageNumber);
-    settableData(tableData);
-  }
-
-  useEffect(() => {
-    paginate(currentPage);
-  }, [poolshares]);
-
   return (
     <>
       <section>
         <h2>{I18n.t('containers.liquidity.liquidityPage.yourLiquidity')}</h2>
         <Row>
-          {tableData.map((poolpair, i) => (
+          {poolshares.map((poolpair, i) => (
             <Col md='6' key={i}>
               <LiquidityAccordion poolpair={poolpair} history={props.history} />
             </Col>
