5e125830488fb6f825970e5b1330ec8fc2b17f26
Harsh <harshrathi.dev@gmail.com>
2022-04-21 14:20:27 +0530

    Removed slippage btn

--- webapp/src/components/SlippageTolerance/index.tsx
+++ webapp/src/components/SlippageTolerance/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BigNumber from 'bignumber.js';
 import { I18n } from 'react-redux-i18n';
 import {
@@ -7,14 +7,10 @@ import {
   Input,
   InputGroup,
   InputGroupAddon,
-  Label,
   Row,
 } from 'reactstrap';
 import { debounce } from 'lodash';
-import { MdEdit, MdClose } from 'react-icons/md';
-import styles from './SlippageTolerance.module.scss';
-
-const percentageList = ['0.5', '1', '3', '5', '10'];
+import { MdClose } from 'react-icons/md';
 
 interface SlippageToleranceProps {
   slippage: BigNumber;
@@ -29,16 +25,9 @@ export default function SlippageTolerance({
   slippageError,
   setSlippageError,
 }: SlippageToleranceProps): JSX.Element {
-  const [isCustomSlippage, setIsCustomSlippage] = useState(false);
-  useEffect(() => {
-    setIsCustomSlippage(
-      !percentageList.includes(new BigNumber(slippage).toString())
-    );
-  }, []);
-
   return (
     <Row>
-      <Col md='4' className='mt-1'>
+      <Col md='4'>
         <span>{I18n.t('containers.swap.slippage.slippageTolerance')}</span>
       </Col>
       <Col md='8'>
@@ -46,8 +35,6 @@ export default function SlippageTolerance({
           slippage={slippage}
           slippageError={slippageError}
           setSlippageError={setSlippageError}
-          isCustomSlippage={isCustomSlippage}
-          setIsCustomSlippage={setIsCustomSlippage}
           onSubmitSlippage={setSlippage}
         />
       </Col>
@@ -56,19 +43,15 @@ export default function SlippageTolerance({
 }
 
 interface SlippageSelectorProps {
-  isCustomSlippage: boolean;
-  onSubmitSlippage: (val: BigNumber, isCustomSlippage: boolean) => void;
-  setIsCustomSlippage: (val: boolean) => void;
+  onSubmitSlippage: (val: BigNumber) => void;
   slippage: BigNumber;
   slippageError?: string;
   setSlippageError: (error?: string) => void;
 }
 
 function SlippageSelector({
-  isCustomSlippage,
   onSubmitSlippage,
   slippage,
-  setIsCustomSlippage,
   slippageError,
   setSlippageError,
 }: SlippageSelectorProps): JSX.Element {
@@ -78,7 +61,7 @@ function SlippageSelector({
 
   const onSlippageChange = (value: string): void => {
     setSelectedSlippage(value);
-    submitSlippage(new BigNumber(value), isCustomSlippage);
+    submitSlippage(new BigNumber(value));
   };
 
   const validateSlippage = (value: string): void => {
@@ -105,50 +88,29 @@ function SlippageSelector({
 
   return (
     <div className='mb-4'>
-      <Row className='px-3'>
-        {percentageList.map((value) => (
-          <SlippageButton
-            key={value}
-            onPress={() => {
-              setIsCustomSlippage(false);
-              onSlippageChange(value);
-            }}
-            isActive={!isCustomSlippage && selectedSlippage === value}
-            label={`${value}%`}
-          />
-        ))}
-
-        {isCustomSlippage ? (
-          <div className={`mt-1 {styles.inline}`}>
-            <InputGroup>
-              <Input
-                type='text'
-                placeholder={'0.00%'}
-                name='toAddress'
-                id='toAddress'
-                value={selectedSlippage.toString()}
-                onChange={(e) => onSlippageChange(e.target.value)}
-              />
-              <InputGroupAddon addonType='append'>
-                <Button
-                  color='outline-primary'
-                  onClick={() => onSlippageChange('')}
-                >
-                  <MdClose />
-                </Button>
-              </InputGroupAddon>
-            </InputGroup>
-          </div>
-        ) : (
-          <SlippageButton
-            onPress={() => {
-              setIsCustomSlippage(true);
-            }}
-            icon={<MdEdit size={10} />}
-            isActive={isCustomSlippage}
-            label={I18n.t('containers.swap.slippage.custom')}
-          />
-        )}
+      <Row>
+        <Col md='4'>
+          <InputGroup>
+            <Input
+              type='number'
+              max={100}
+              min={0}
+              placeholder={'0.00%'}
+              name='toAddress'
+              id='toAddress'
+              value={selectedSlippage.toString()}
+              onChange={(e) => onSlippageChange(e.target.value)}
+            />
+            <InputGroupAddon addonType='append'>
+              <Button
+                color='outline-primary'
+                onClick={() => onSlippageChange('')}
+              >
+                <MdClose />
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Col>
       </Row>
 
       {isRiskWarningDisplayed && (
@@ -157,32 +119,6 @@ function SlippageSelector({
         </div>
       )}
       {slippageError && <div className='text-danger mt-2'>{slippageError}</div>}
-    </div>
-  );
-}
-
-function SlippageButton({
-  onPress,
-  isActive,
-  label,
-  icon,
-}: {
-  onPress: () => void;
-  isActive: boolean;
-  label: string;
-  icon?: ReactElement;
-}): JSX.Element {
-  return (
-    <div className={styles.inline}>
-      <Button
-        color='primary'
-        className='mr-1 mt-1'
-        outline={!isActive}
-        onClick={onPress}
-      >
-        {icon}
-        <div className={styles.inline}>{label}</div>
-      </Button>
     </div>
   );
 }
